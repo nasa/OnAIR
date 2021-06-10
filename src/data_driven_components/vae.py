a9103fd892cef8bd3758896eafa31cab93566a40
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2021-06-10 14:59:27 -0400

    Working VAE forward + docstring

--- src/data_driven_components/vae.py
+++ src/data_driven_components/vae.py
@@ -1,18 +1,23 @@
 """
 LSTM-VAE for streamed satellite telemetry fault diagnosis
-Gabriel Rasskin @ NASA
 """
 import torch
 from torch import nn
 
 class VAE(nn.Module):
-    def __init__(self, z_units=5, input_dim=30, seq_len=15):
+    def __init__(self, input_dim=30, seq_len=15, z_units=5):
+        """
+        LSTM-VAE class for anomaly detection and diagnosis
+        :param input_dim: (int) number of input features
+        :param seq_len: (int) number of data points in our data sequence
+        :param z_units: (int) dimensions of our latent space gaussian representation
+        """
         super(VAE, self).__init__()
 
         self.input_dim = input_dim
         self.hidden_dim = z_units * 2 # good rule of thumb
         self.z_units = z_units
-        self.seq_len = seq_len
+        self.seq_len = seq_len # TODO: maybe detect automatically from data?
 
         # batch_first = true makes output tensor of size (batch, seq, feature).
         self.enc1 = nn.LSTM(
@@ -27,6 +32,9 @@ class VAE(nn.Module):
             num_layers=1,
             batch_first=True
         )
+
+        self.mulinear = nn.Linear(self.z_units, self.z_units)
+        self.logvarlinear = nn.Linear(self.z_units, self.z_units)
 
         self.dec1 = nn.LSTM(
             input_size=self.z_units,
@@ -44,28 +52,76 @@ class VAE(nn.Module):
         self.output_layer = nn.Linear(self.input_dim, self.input_dim) # TODO: add activation, maybe relu?
 
     def encoder(self, x):
+        """
+        :param x: (Tensor) input sequence of shape (batch_size, seq_len, input_dim)
+        :return: (average, logvar) each of shape (batch_size, 1, z_units)
+        """
         x = x.reshape((1, self.seq_len, self.input_dim)) # TODO: should move to data processing
         # LSTM output is tuple (output, (hidden state, cell state))
         x, (_, _) = self.enc1(x)
         _, (hidden_state, _) = self.enc2(x)
-        # hidden state of last lstm is latent representation
-        return hidden_state.reshape((self.input_dim, self.z_units)) # TODO: cleanup dims
+        mu = self.mulinear(hidden_state)
+        logvar = self.logvarlinear(hidden_state)
+        
+        return (mu, logvar)
+
+    def reparametrize(self, mu, logvar):
+        """
+        :param mu: (Tensor) Latent space average tensor of shape (batch_size, 1, z_units)
+        :param logvar: (Tensor) Logvariance of representation, shape (batch_size, 1, z_units)
+        :return: (Tensor) Sampling from our Gaussian (batch_size, 1, z_units)
+        """
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps * std + mu
 
     def decoder(self, x):
-        # TODO: deal with x reshaping
+        """
+        :param x: (Tensor) sampling sequence of shape (batch_size, seq_len, z_units)
+        :return: (Tensor) output sequence of shape (batch_size, seq_len, input_dim)
+        """
         x, (_, _) = self.dec1(x)
         x, (_, _) = self.dec2(x)
         # TODO: deal with x reshaping
         return self.output_layer(x)
 
     def forward(self, x):
-        x = self.encoder(x)
+        """
+        :param x: (Tensor) input sequence of shape (batch_size, seq_len, input_dim)
+        :return: (Tensor) output sequence of shape (batch_size, seq_len, input_dim)
+        """
+        self.input = x
+        mu, logvar = self.encoder(x)
+        self.mu = mu
+        self.logvar = logvar
+        x = self.reparametrize(mu, logvar)
+        x = x.repeat(1,self.seq_len,1)
         x = self.decoder(x)
+        return x
 
-# TODO: add loss criterion bce and recontruction error
+    def loss(self, x, mu, logvar, output):
+        """
+        Combination of mse (reconstruction error) between input and output and 
+            KL-divergence of N(0,1) and N(mu, logvar)
+        :param x: (Tensor) input sequence of shape (batch_size, seq_len, input_dim)
+        :param mu: (Tensor) Latent space average tensor of shape (batch_size, 1, z_units)
+        :param logvar: (Tensor) Logvariance of representation, shape (batch_size, 1, z_units)
+        :param output: mse+KL divergence loss
+        """
+        num_batches = x.shape[0]
+        mse_loss = nn.functional.mse_loss(x, output)
+        mse_loss = torch.mean(mse_loss, dim=0)
+        kldivergence_loss = torch.mean(-0.5 * torch.sum(1 + logvar - mu ** 2 - logvar.exp(), dim = 1), dim = 0)
+        # TODO: double check batch size averaging
+        return mse_loss + kldivergence_loss
 
 if __name__ == "__main__":
     print("Creating VAE...")
-    vae = VAE()
+    vae = VAE(input_dim=30, seq_len=1, z_units=5)
     # TODO: training loop
     print("Successfuly created VAE")
+    data = range(30)
+    data = [float(x) for x in data]
+    x = torch.tensor([[data]])
+    out = vae(x)
+    print("Input data: {}\nOutput reconstruction: {}".format(x, out))
