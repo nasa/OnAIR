89328c8fb4db2f4249395226715b244abfb05ed3
Hayley Owens <hayleyjeanowens@gmail.com>
2021-06-17 10:07:29 -0500

    Removing comment

--- driver.py
+++ driver.py
@@ -12,7 +12,7 @@ import pathlib
 from datetime import datetime
 from test_all import *
 
-#from src.run_scripts.execution_engine import ExecutionEngine
+from src.run_scripts.execution_engine import ExecutionEngine
 
 def main():
     os.system('find . -name \'.DS_Store\' -type f -delete') # Removes those pesky .DS_Store files that Macs make
