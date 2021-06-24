b841b9ffbc467e148cc7738958f09ab6a87357fd
Evana Gizzi <egizzi@GSLAL0320070003.local>
2021-06-24 11:44:16 -0400

    added in the new assoc components, not integrated yet. Needs to have tests written

---
+++ src/data_driven_components/associativity/associativity_data_manager.py
@@ -0,0 +1,64 @@
+'''
+This class processes the raw sensor data frames into a format
+that is usable by the associativity algorithm
+
+Author: Chris Trombley
+June 10th 2021
+'''
+
+import numpy as np
+import math
+import keras
+
+class AssociativityDataManager:
+    def __init__(self):
+        self.window_size = 20
+        self.frame_id = 0
+        self.datum_id = 0
+        self.frames = []
+        self.sensor_data = []
+        self.categorical_data = []
+        self.categorical_data_record = []
+        self.sensor_idx_mapping = {3: 'counter_3', 4: 'Voltage', 5: 'Current', 6: 'counter', 7: 'internal temp', 8: 'extenal temp', 9: 'counter_2', 10: 'lat', 11: 'long', 12: 'alt'} ##this could be better
+        self.model_path = '/home/chris/Desktop/RAISR-2.0/src/util/data_generation_scripts/models'
+        self.model = keras.models.load_model(self.model_path)
+        self.class_index_mapping = {0: 'linear increasing', 1: 'linear decrease', 2: 'constant', 3: 'sin'}
+
+    def convert_frames_to_categorical(self):
+        for sensor_idx in range(len(self.frames[0])):
+            self.categorical_data_record = []
+            self.input_data = []
+            for data_row in range(len(self.frames)): ##assumes all are the same size
+                if sensor_idx < 4 or sensor_idx == 6 or sensor_idx == 9: ##hacky - change
+                    continue
+                else:
+                    self.input_data.append(float(self.frames[data_row][sensor_idx]))
+                    delta = 0
+                    if delta == 0:
+                        self.categorical_data_record.append(str(self.sensor_idx_mapping[sensor_idx]) + ' constant')    
+                    elif delta > 0:
+                        self.categorical_data_record.append(str(self.sensor_idx_mapping[sensor_idx]) + ' increasing')
+                    elif delta < 0:
+                        self.categorical_data_record.append(str(self.sensor_idx_mapping[sensor_idx]) + ' decreasing')
+            print('-'*30)
+
+            if sensor_idx >= 4 and sensor_idx != 6 and sensor_idx != 9: ##hacky - change 
+                input_data_np = np.array(self.input_data).reshape(1, 20, 1)
+                preds = self.model.predict([input_data_np, input_data_np, input_data_np]) ## do we need to do midpt preprocessing first?
+                max_pre_prob = max(preds.tolist())
+                max_pre_prob_idx = preds.tolist().index(max_pre_prob)
+
+            self.categorical_data.append(self.categorical_data_record)
+        
+    def add_frame(self, frame):
+        self.frames.append(frame)
+        self.frame_id += 1
+        print(len(self.frames))
+        if int(self.frame_id / self.window_size) >= 1:
+            self.convert_frames_to_categorical()
+            
+    def get_data(self):
+        if len(self.categorical_data) > 0:
+            return self.categorical_data    
+        else:    
+            return -1
