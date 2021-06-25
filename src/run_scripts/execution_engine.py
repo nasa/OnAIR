46f3c833a71efc66f1119ebde9ee342389651fb0
Marshall, James C 459937851 <james.marshall-1@nasa.gov>
2021-06-25 12:24:42 -0400

    leaves the config path relative to the working directory

--- src/run_scripts/execution_engine.py
+++ src/run_scripts/execution_engine.py
@@ -54,9 +54,7 @@ class ExecutionEngine:
 
     def parse_configs(self, config_file_path):
         # print("Using config file: {}".format(config_file_path))
-        if not os.path.isabs(config_file_path):
-            config_file_path = os.path.join(os.environ['SRC_ROOT_PATH'], config_file_path)
-            
+
         config = configparser.ConfigParser()
         config.read(config_file_path)
         ## Sort Data: Telementry Data & Configuration
