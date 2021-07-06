a64527a1749e262e71f5cab9ae8630f7a74d2e03
Hayley Owens <hayleyjeanowens@gmail.com>
2021-07-06 10:46:36 -0500

    Allow multiple files in 42 format

--- src/test/data_handling/parsers/test_generic_parser.py
+++ src/test/data_handling/parsers/test_generic_parser.py
@@ -49,17 +49,34 @@ class TestGenericParser(unittest.TestCase):
                                                   'description_assignments': {fileName: ['No description', 'No description', 'No description', 'No description']}})        
 
 
-    # def test_multisource_parse_sim_data(self):
-    #     multi_source_data_files = [str(['generic_test_42.txt', 'generic_test_42_file2.txt'])]
-    #     multi_source_config_files = [str(['generic_test_42_CONFIG.txt', 'generic_test_42_file2_CONFIG.txt'])]
-    #     for i in range(len(self.parser_names)):
-    #         parser = importlib.import_module('src.data_handling.parsers.' + self.parser_file_names[i])
-    #         parser_class = getattr(parser, self.parser_names[i])
-    #         P = parser_class(self.rawDataFilepath, 
-    #                          self.tlmConfigFilepath,
-    #                          multi_source_data_files[i],
-    #                          multi_source_config_files[i])
-    #         headers, sim_data, configs = P.get_sim_data()
+    def test_multisource_parse_sim_data(self):
+        multi_source_data_files = [str(['generic_test_42.txt', 'generic_test_42_file2.txt'])]
+        multi_source_config_files = [str(['generic_test_42_CONFIG.txt', 'generic_test_42_file2_CONFIG.txt'])]
+        for i in range(len(self.parser_names)):
+            parser = importlib.import_module('src.data_handling.parsers.' + self.parser_file_names[i])
+            parser_class = getattr(parser, self.parser_names[i])
+            P = parser_class(self.rawDataFilepath, 
+                            self.tlmConfigFilepath,
+                            multi_source_data_files[i],
+                            multi_source_config_files[i])
+            headers, sim_data, configs = P.get_sim_data()
+            fileName1 = ast.literal_eval(multi_source_data_files[i])[0]
+            fileName2 = ast.literal_eval(multi_source_data_files[i])[1]
+            self.assertEquals(P.all_headers, { fileName1 : ['TIME', 'A', 'B', 'C'], fileName2 : ['TIME', 'D', 'E']})
+            self.assertEquals(P.sim_data, {'1000': { fileName1 : ['1000', '0', '0.000000000000e+00', '0.0'], fileName2 : ['1000','0', '0.000000000000e+00']}, 
+                                           '1001': { fileName1 : ['1001', '1', '1.000000000000e+00', '1.0']}, 
+                                           '1002': { fileName1 : ['1002', '2', '2.000000000000e+00', '2']}, 
+                                           '1003': { fileName1 : ['1003', '3', '3.000000000000e+00', '3'], fileName2 : ['1003','1', '1.000000000000e+00']},
+                                           '1004': { fileName2 : ['1004','2', '2.000000000000e+00']}})
+            self.assertEquals(P.binning_configs, {'subsystem_assignments': {fileName1: [['MISSION'], ['MISSION'], ['MISSION'], ['MISSION']], 
+                                                                            fileName2 : [['MISSION'], ['MISSION'], ['MISSION']]}, 
+                                                  'test_assignments': {fileName1: [[['SYNC', 'TIME']], [['FEASIBILITY', -1.0, 0.0, 10.0, 15.0]], [['NOOP']], [['FEASIBILITY', -1.0, 0.0, 10.0, 15.0]]], 
+                                                                        fileName2 : [[['SYNC', 'TIME']], [['FEASIBILITY',-1.0, 0.0, 10.0, 15.0]], [['NOOP']]]}, 
+                                                  'description_assignments': {fileName1: ['No description', 'No description', 'No description', 'No description'], 
+                                                                                fileName2 : ['No description', 'No description', 'No description']}})
+        
+        
+        
 
 
 if __name__ == '__main__':
