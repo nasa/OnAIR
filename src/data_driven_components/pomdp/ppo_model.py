471b41cd6a5c096704d52793e54acab336a22bc2
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2021-07-08 11:42:26 -0400

    PPO model load

--- src/data_driven_components/pomdp/ppo_model.py
+++ src/data_driven_components/pomdp/ppo_model.py
@@ -6,33 +6,31 @@ import os
 
 class PPOModel(DataLearner):
 
-    def __init__(self):
+    def __init__(self, window_size, config_path='src/data/raw_telemetry_data/data_physics_generation/Errors/config.csv', model_path='src/data_driven_components/pomdp/models'):
         """
-        :param headers: (string list) list of headers for each input feature
-        :param window_size: (int) number of data points in our data sequence
-        :param z_units: (int) dimensions of our latent space gaussian representation
-        :param hidden_units: (int) dimension of our hidden_units
-        :param path: (string) path of vae save relative to src directory
+        :param window_size: (int) length of time agent examines
+        :param config_path: (optional String) path to PPO config
+        :param model_path: (optional String) path to saved model
         """
         self.frames = []
-        data_path = os.path.join(os.environ['SRC_ROOT_PATH'], 'src/data/raw_telemetry_data/data_physics_generation/Errors')
-        dict_config, data = mass_load_data(data_path, lookback=15)
-        data = stratified_sampling(dict_config, data)
-        training_data = data[:int(len(data)*(0.7))]
-        testing_data = data[int(len(data)*(0.7)):]
+        model_path = os.path.join(os.environ['SRC_ROOT_PATH'], model_path)
+        config_path = os.path.join(os.environ['SRC_ROOT_PATH'], config_path)
 
-        model_path = os.path.join(os.environ['SRC_ROOT_PATH'], 'src/data_driven_components/pomdp/models')
-        config_path = os.path.join(os.environ['SRC_ROOT_PATH'], 'src/data/raw_telemetry_data/data_physics_generation/Errors/config.csv')
-        agent = PPO('ppo_train', model_path, config_path=config_path)
-        agent.train_ppo(training_data, testing_data, 1090)
+        self.agent = PPO('ppo_train', model_path, config_path=config_path)
 
-    def apriori_training(self, data_train):
+    def apriori_training(self, data):
         """
         Given data, system should learn any priors necessary for realtime diagnosis.
         :param data_train: (Tensor) shape (batch_size, seq_size, feat_size)
         # TODO: double check sizes
         """
-        pass
+        #data_path = os.path.join(os.environ['SRC_ROOT_PATH'], 'src/data/raw_telemetry_data/data_physics_generation/Errors')
+        #dict_config, data = mass_load_data(data_path, lookback=15)
+        data = stratified_sampling(dict_config, data)
+        training_data = data[:int(len(data)*(0.7))]
+        testing_data = data[int(len(data)*(0.7)):]
+
+        agent.train_ppo(training_data, testing_data, 1090)
 
     def update(self, frame):
         """
