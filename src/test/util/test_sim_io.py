b4b5487de92c478798c839549f1a797419d79521
asgibso1 <alan.s.gibson@nasa.gov>
2022-10-27 14:59:24 -0400

    WIP:Added tests for render_viz
    Using MagicMock for randomized objects

--- src/test/util/test_sim_io.py
+++ src/test/util/test_sim_io.py
@@ -1,5 +1,131 @@
-from mock import patch
+from mock import MagicMock
 import util.sim_io
+
+def test_render_viz_does_only_stattest_render_viz_does_status_sensor_and_diagnosis_reports_when_diagnosis_is_givenus_and_sensor_reports_when_diagnosis_is_not_given(mocker):
+  # Arrange
+  SAVE_PATH = 'RAISR_VIZ_SAVE_PATH'
+  arg_status_data = MagicMock()
+  arg_sensor_data = MagicMock()
+  arg_sim_name = MagicMock()
+
+  fake_system_filename = MagicMock()
+  fake_full_path = MagicMock()
+  fake_iterator = MagicMock()
+  fake_file = MagicMock()
+  fake_file.configure_mock(**{'__enter__.return_value': fake_iterator})
+
+  expected_status_report = {}
+  expected_status_report['filename'] = arg_sim_name
+  expected_status_report['data'] = arg_status_data
+  expected_sensor_status_report = {}
+  expected_sensor_status_report['name'] = 'MISSION'
+  expected_sensor_status_report['children'] = arg_sensor_data
+
+  mocker.patch('util.sim_io.os.environ.get', return_value=fake_system_filename)
+  mocker.patch('util.sim_io.os.path.join', return_value=fake_full_path)
+  mocker.patch('builtins.open', return_value=fake_file)
+  mocker.patch('util.sim_io.json.dump')
+
+  # Act
+  util.sim_io.render_viz(arg_status_data, arg_sensor_data, arg_sim_name)
+
+  # Assert
+  assert util.sim_io.os.environ.get.call_args_list[0].args == (SAVE_PATH,)
+  assert util.sim_io.os.path.join.call_args_list[0].args == (fake_system_filename, 'system.json')
+  assert open.call_args_list[0].args == (fake_full_path, 'w')
+  assert util.sim_io.json.dump.call_args_list[0].args == (expected_status_report, fake_iterator)
+  assert util.sim_io.os.environ.get.call_args_list[1].args == (SAVE_PATH,)
+  assert util.sim_io.os.path.join.call_args_list[1].args == (fake_system_filename, 'faults.json')
+  assert open.call_args_list[1].args == (fake_full_path, 'w')
+  assert util.sim_io.json.dump.call_args_list[1].args == (expected_sensor_status_report, fake_iterator)
+  assert len(open.call_args_list) == 2 
+  
+def test_render_viz_does_only_status_and_sensor_reports_when_diagnosis_is_given_as_None(mocker):
+  # Arrange
+  SAVE_PATH = 'RAISR_VIZ_SAVE_PATH'
+  arg_status_data = MagicMock()
+  arg_sensor_data = MagicMock()
+  arg_sim_name = MagicMock()
+  arg_diagnosis = None
+
+  fake_system_filename = MagicMock()
+  fake_full_path = MagicMock()
+  fake_iterator = MagicMock()
+  fake_file = MagicMock()
+  fake_file.configure_mock(**{'__enter__.return_value': fake_iterator})
+
+  expected_status_report = {}
+  expected_status_report['filename'] = arg_sim_name
+  expected_status_report['data'] = arg_status_data
+  expected_sensor_status_report = {}
+  expected_sensor_status_report['name'] = 'MISSION'
+  expected_sensor_status_report['children'] = arg_sensor_data
+
+  mocker.patch('util.sim_io.os.environ.get', return_value=fake_system_filename)
+  mocker.patch('util.sim_io.os.path.join', return_value=fake_full_path)
+  mocker.patch('builtins.open', return_value=fake_file)
+  mocker.patch('util.sim_io.json.dump')
+
+  # Act
+  util.sim_io.render_viz(arg_status_data, arg_sensor_data, arg_sim_name, arg_diagnosis)
+
+  # Assert
+  assert util.sim_io.os.environ.get.call_args_list[0].args == (SAVE_PATH,)
+  assert util.sim_io.os.path.join.call_args_list[0].args == (fake_system_filename, 'system.json')
+  assert open.call_args_list[0].args == (fake_full_path, 'w')
+  assert util.sim_io.json.dump.call_args_list[0].args == (expected_status_report, fake_iterator)
+  assert util.sim_io.os.environ.get.call_args_list[1].args == (SAVE_PATH,)
+  assert util.sim_io.os.path.join.call_args_list[1].args == (fake_system_filename, 'faults.json')
+  assert open.call_args_list[1].args == (fake_full_path, 'w')
+  assert util.sim_io.json.dump.call_args_list[1].args == (expected_sensor_status_report, fake_iterator)
+  assert len(open.call_args_list) == 2 
+  
+def test_render_viz_does_status_sensor_and_diagnosis_reports_when_diagnosis_is_given(mocker):
+  # Arrange
+  SAVE_PATH = 'RAISR_VIZ_SAVE_PATH'
+  arg_status_data = MagicMock()
+  arg_sensor_data = MagicMock()
+  arg_sim_name = MagicMock()
+  arg_diagnosis = MagicMock()
+
+  fake_system_filename = MagicMock()
+  fake_full_path = MagicMock()
+  fake_iterator = MagicMock()
+  fake_file = MagicMock()
+  fake_file.configure_mock(**{'__enter__.return_value': fake_iterator})
+  fake_results = MagicMock()
+
+  expected_status_report = {}
+  expected_status_report['filename'] = arg_sim_name
+  expected_status_report['data'] = arg_status_data
+  expected_sensor_status_report = {}
+  expected_sensor_status_report['name'] = 'MISSION'
+  expected_sensor_status_report['children'] = arg_sensor_data
+
+  mocker.patch('util.sim_io.os.environ.get', return_value=fake_system_filename)
+  mocker.patch('util.sim_io.os.path.join', return_value=fake_full_path)
+  mocker.patch('builtins.open', return_value=fake_file)
+  mocker.patch('util.sim_io.json.dump')
+  arg_diagnosis.configure_mock(**{'get_diagnosis_viz_json.return_value': fake_results})
+
+  # Act
+  util.sim_io.render_viz(arg_status_data, arg_sensor_data, arg_sim_name, arg_diagnosis)
+
+  # Assert
+  assert util.sim_io.os.environ.get.call_args_list[0].args == (SAVE_PATH,)
+  assert util.sim_io.os.path.join.call_args_list[0].args == (fake_system_filename, 'system.json')
+  assert open.call_args_list[0].args == (fake_full_path, 'w')
+  assert util.sim_io.json.dump.call_args_list[0].args == (expected_status_report, fake_iterator)
+  assert util.sim_io.os.environ.get.call_args_list[1].args == (SAVE_PATH,)
+  assert util.sim_io.os.path.join.call_args_list[1].args == (fake_system_filename, 'faults.json')
+  assert open.call_args_list[1].args == (fake_full_path, 'w')
+  assert util.sim_io.json.dump.call_args_list[1].args == (expected_sensor_status_report, fake_iterator)
+  arg_diagnosis.get_diagnosis_viz_json.called_once()
+  assert util.sim_io.os.environ.get.call_args_list[2].args == (SAVE_PATH,)
+  assert util.sim_io.os.path.join.call_args_list[2].args == (fake_system_filename, 'results.json')
+  assert open.call_args_list[2].args == (fake_full_path, 'w')
+  assert util.sim_io.json.dump.call_args_list[2].args == (fake_results, fake_iterator)
+  assert len(open.call_args_list) == 3
 
 def test_print_dots_uses_mod_10_plus_one_dots_when_ts_mod_20_is_less_than_10(mocker):
   # Arrange
