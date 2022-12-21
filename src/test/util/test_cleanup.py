cd7a663d99638808eff8149d3511c3e685b9463e
Evana Gizzi <egizzi@GSLAL0320070003.local>
2022-12-21 12:19:01 -0500

    failed CI because of a bad import, apparently there were a few

--- src/test/util/test_cleanup.py
+++ src/test/util/test_cleanup.py
@@ -19,13 +19,13 @@ def test_clean_all_removes_provided_file_types_back_two_directories_from_file_lo
   util.cleanup.clean_all(arg_run_path)
 
   # Assert
-  assert util.cleanup.os.path.realpath.call_count == 1
-  assert util.cleanup.os.path.dirname.call_count == 1
-  assert util.cleanup.os.chdir.call_count == 1
-  assert util.cleanup.os.chdir.call_args_list[0].args == (fake_run_path + '/../../',)
-  assert util.cleanup.os.system.call_count == 2
-  assert util.cleanup.os.system.call_args_list[0].args == ('find . | grep -E "(__pycache__|.pyc|.pyo$)" | xargs rm -rf', )
-  assert util.cleanup.os.system.call_args_list[1].args == ('find . | grep -E ".DS_Store" | xargs rm -rf', )
+  assert src.util.cleanup.os.path.realpath.call_count == 1
+  assert src.util.cleanup.os.path.dirname.call_count == 1
+  assert src.util.cleanup.os.chdir.call_count == 1
+  assert src.util.cleanup.os.chdir.call_args_list[0].args == (fake_run_path + '/../../',)
+  assert src.util.cleanup.os.system.call_count == 2
+  assert src.util.cleanup.os.system.call_args_list[0].args == ('find . | grep -E "(__pycache__|.pyc|.pyo$)" | xargs rm -rf', )
+  assert src.util.cleanup.os.system.call_args_list[1].args == ('find . | grep -E ".DS_Store" | xargs rm -rf', )
 
 def test_clean_all_removes_provided_file_types_from_given_run_path_when_it_is_not_empty(mocker):
   # Arrange
@@ -64,9 +64,9 @@ def test_setup_folders_creates_dir_when_given_results_path_does_not_exist(mocker
   util.cleanup.setup_folders(arg_results_path)
   
   # Assert
-  assert util.cleanup.os.path.isdir.call_count == 1
-  assert util.cleanup.os.mkdir.call_count == 1
-  assert util.cleanup.os.mkdir.call_args_list[0].args == (arg_results_path, )
+  assert src.util.cleanup.os.path.isdir.call_count == 1
+  assert src.util.cleanup.os.mkdir.call_count == 1
+  assert src.util.cleanup.os.mkdir.call_args_list[0].args == (arg_results_path, )
 
 def test_setup_folders_does_not_create_dir_when_it_already_exists(mocker):
   # Arrange
@@ -79,5 +79,5 @@ def test_setup_folders_does_not_create_dir_when_it_already_exists(mocker):
   util.cleanup.setup_folders(arg_results_path)
   
   # Assert
-  assert util.cleanup.os.path.isdir.call_count == 1
-  assert util.cleanup.os.mkdir.call_count == 0
+  assert src.util.cleanup.os.path.isdir.call_count == 1
+  assert src.util.cleanup.os.mkdir.call_count == 0
