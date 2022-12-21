06af843d62a1e930a4969bedb9b390fbdecece99
asgibso1 <alan.s.gibson@nasa.gov>
2022-12-21 14:55:58 -0500

    extract_configs tests

--- src/test/data_handling/parsers/test_parser_util.py
+++ src/test/data_handling/parsers/test_parser_util.py
@@ -1,29 +1,119 @@
 """ Test Parser Util Functionality """
-import os
-# import sys
-import unittest
-# import importlib
+import pytest
+from mock import MagicMock
+import src.data_handling.parsers.parser_util as parser_util
 
-from src.data_handling.parsers.parser_util import *
 
-class TestParserUtil(unittest.TestCase):
+# extract_configs tests
+def test_extract_configs_returns_empty_dicts_when_given_configFiles_is_vacant():
+    # Arrange
+    arg_configFilePath = MagicMock()
+    arg_configFiles = []
+    arg_csv = MagicMock()
 
-    def setUp(self):
-        self.run_path = os.environ['RUN_PATH']
+    expected_result = {'subsystem_assignments' : {},
+                       'test_assignments' : {},
+                       'description_assignments' : {}}
 
-    def test_extract_configs(self):
-        return
+    # Act
+    result = parser_util.extract_configs(arg_configFilePath, arg_configFiles, arg_csv)
 
-    def test_extract_config(self):
-        return
+    # Assert
+    assert result == expected_result
 
-    def test_str2lst(self):
-        return
+def test_extract_configs_returns_expected_dicts_dict_when_configFiles_has_one_cFile(mocker):
+    # Arrange
+    arg_configFilePath = MagicMock()
+    arg_configFiles = [MagicMock()]
+    arg_csv = MagicMock()
 
-    def test_process_filepath(self):
-        return
+    fake_filename = MagicMock()
+    fake_subsystem_assignments = MagicMock()
+    fake_tests = MagicMock()
+    fake_descs = MagicMock()
 
-if __name__ == '__main__':
-    unittest.main()
+    forced_return_extract_config = [fake_filename, fake_subsystem_assignments, fake_tests, fake_descs]
+
+    mocker.patch('src.data_handling.parsers.parser_util.extract_config', return_value=forced_return_extract_config)
+
+    expected_result = {'subsystem_assignments' : {fake_filename:fake_subsystem_assignments},
+                       'test_assignments' : {fake_filename:fake_tests},
+                       'description_assignments' : {fake_filename:fake_descs}}
+
+    # Act
+    result = parser_util.extract_configs(arg_configFilePath, arg_configFiles, arg_csv)
+
+    # Assert
+    assert parser_util.extract_config.call_count == 1
+    assert parser_util.extract_config.call_args_list[0].args == (arg_configFilePath, arg_configFiles[0], )
+    assert parser_util.extract_config.call_args_list[0].kwargs == {'csv':arg_csv}
+    assert result == expected_result
+
+def test_extract_configs_returns_expected_dicts_dict_when_configFiles_has_many_cFile(mocker):
+    # Arrange
+    arg_configFilePath = MagicMock()
+    arg_configFiles = [] 
+    arg_csv = MagicMock()
+
+    num_fake_cFiles = pytest.gen.randint(2, 10) # arbitrary, from 2 to 10 (0 and 1 have own tests)
+
+    fake_filenames = [MagicMock()] * num_fake_cFiles
+    fake_subsystem_assignments = [MagicMock()] * num_fake_cFiles
+    fake_tests = [MagicMock()] * num_fake_cFiles
+    fake_descs = [MagicMock()] * num_fake_cFiles
+
+    forced_return_extract_configs = []
+    for i in range(num_fake_cFiles):
+        arg_configFiles.append(MagicMock())
+        forced_return_extract_configs.append([fake_filenames[i], fake_subsystem_assignments[i], fake_tests[i], fake_descs[i]])
+
+    mocker.patch('src.data_handling.parsers.parser_util.extract_config', side_effect=forced_return_extract_configs)
+
+    expected_result = {}
+    expected_result['subsystem_assignments'] = {fake_filenames[0]:fake_subsystem_assignments[0]}
+    expected_result['test_assignments'] = {fake_filenames[0]:fake_tests[0]}
+    expected_result['description_assignments'] = {fake_filenames[0]:fake_descs[0]}
+    for i in range(1, num_fake_cFiles):
+        expected_result['subsystem_assignments'][fake_filenames[i]] = fake_subsystem_assignments[i]
+        expected_result['test_assignments'][fake_filenames[i]] = fake_tests[i]
+        expected_result['description_assignments'][fake_filenames[i]] = fake_descs[i]
+
+    # Act
+    result = parser_util.extract_configs(arg_configFilePath, arg_configFiles, arg_csv)
+
+    # Assert
+    assert parser_util.extract_config.call_count == num_fake_cFiles
+    for i in range(num_fake_cFiles):
+        assert parser_util.extract_config.call_args_list[i].args == (arg_configFilePath, arg_configFiles[i], )
+        assert parser_util.extract_config.call_args_list[i].kwargs == {'csv':arg_csv}
+    assert result == expected_result
+
+# extract_config tests
+
+# str2lst tests
+
+# process_filepath
+
+
+
+# class TestParserUtil(unittest.TestCase):
+
+#     def setUp(self):
+#         self.run_path = os.environ['RUN_PATH']
+
+#     def test_extract_configs(self):
+#         return
+
+#     def test_extract_config(self):
+#         return
+
+#     def test_str2lst(self):
+#         return
+
+#     def test_process_filepath(self):
+#         return
+
+# if __name__ == '__main__':
+#     unittest.main()
 
 
