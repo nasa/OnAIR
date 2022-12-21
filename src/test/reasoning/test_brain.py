3f37ed19057356ed3f9d9880d1107bcfaf76ac69
asgibso1 <alan.s.gibson@nasa.gov>
2022-12-21 18:10:39 -0500

    Removed commented code

--- src/test/reasoning/test_brain.py
+++ src/test/reasoning/test_brain.py
@@ -78,28 +78,3 @@ def test_diagnose_returns_None():
 
     # Assert
     assert result == None
-
-# class TestBrain(unittest.TestCase):
-
-#     def setUp(self):
-#         self.test_path = os.path.dirname(os.path.abspath(__file__))
-#         SC = Spacecraft(['TIME', 'A', 'B'], [[['SYNC', 'TIME']], [['NOOP']], [['NOOP']]])
-#         self.B = Brain(SC)
-
-#     def test_init_nonempty_brain(self):
-#         self.assertEqual(type(self.B.spacecraft_rep), Spacecraft)
-#         self.assertEqual(self.B.mission_status, '---')
-#         self.assertEqual(self.B.bayesian_status, ('---', -1.0))
-
-#     def test_reason(self):
-#         frame = [1, 1, 1]
-#         self.B.reason(frame)
-#         self.assertEqual(self.B.spacecraft_rep.get_current_data(), [1,1,1])
-#         self.assertEqual(self.B.mission_status, 'GREEN')
-
-#     def test_diagnose(self):
-#         return
-        
-
-# if __name__ == '__main__':
-#     unittest.main()
