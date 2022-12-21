faa6b3172e97bbb18ada1e2a5fe1c7a5556650c5
asgibso1 <alan.s.gibson@nasa.gov>
2022-12-21 17:51:01 -0500

    reason tests

--- src/test/reasoning/test_brain.py
+++ src/test/reasoning/test_brain.py
@@ -8,7 +8,31 @@ from src.reasoning.brain import Brain
 # __init__ tests
 
 # reason tests
+def test_reason_updates_spacecraft_rep_with_given_frame_and_sets_new_spacecraft_mission_status_and_updates_learning_systems_with_given_frame_and_new_mission_status(mocker):
+    # Arrange
+    arg_frame = MagicMock()
 
+    fake_mission_status = MagicMock()
+
+    cut = Brain.__new__(Brain)
+    cut.spacecraft_rep = MagicMock()
+    cut.learning_systems = MagicMock()
+    
+    mocker.patch.object(cut.spacecraft_rep, 'update')
+    mocker.patch.object(cut.spacecraft_rep, 'get_status', return_value=fake_mission_status)
+    mocker.patch.object(cut.learning_systems, 'update')
+
+    # Act
+    cut.reason(arg_frame)
+
+    # Assert
+    assert cut.spacecraft_rep.update.call_count == 1  
+    assert cut.spacecraft_rep.update.call_args_list[0].args == (arg_frame, ) 
+    assert cut.spacecraft_rep.get_status.call_count == 1  
+    assert cut.spacecraft_rep.get_status.call_args_list[0].args == () 
+    assert cut.learning_systems.update.call_count == 1  
+    assert cut.learning_systems.update.call_args_list[0].args == (arg_frame, fake_mission_status) 
+     
 # diagnose tests
 def test_diagnose_returns_None():
     # Arrange
