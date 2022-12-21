a23d0819097a643736b3bb75f72c8d4cbd123db3
asgibso1 <alan.s.gibson@nasa.gov>
2022-12-21 17:42:02 -0500

    diagnose tests

--- src/test/reasoning/test_brain.py
+++ src/test/reasoning/test_brain.py
@@ -1,31 +1,48 @@
 """ Test Brain Functionality """
-import os
-import unittest
-
-from src.systems.spacecraft import Spacecraft
+import pytest
+from mock import MagicMock
+import src.reasoning.brain as brain
 from src.reasoning.brain import Brain
 
-class TestBrain(unittest.TestCase):
 
-    def setUp(self):
-        self.test_path = os.path.dirname(os.path.abspath(__file__))
-        SC = Spacecraft(['TIME', 'A', 'B'], [[['SYNC', 'TIME']], [['NOOP']], [['NOOP']]])
-        self.B = Brain(SC)
+# __init__ tests
 
-    def test_init_nonempty_brain(self):
-        self.assertEqual(type(self.B.spacecraft_rep), Spacecraft)
-        self.assertEqual(self.B.mission_status, '---')
-        self.assertEqual(self.B.bayesian_status, ('---', -1.0))
+# reason tests
 
-    def test_reason(self):
-        frame = [1, 1, 1]
-        self.B.reason(frame)
-        self.assertEqual(self.B.spacecraft_rep.get_current_data(), [1,1,1])
-        self.assertEqual(self.B.mission_status, 'GREEN')
+# diagnose tests
+def test_diagnose_returns_None():
+    # Arrange
+    arg_time_step = MagicMock()
 
-    def test_diagnose(self):
-        return
+    cut = Brain.__new__(Brain)
+
+    # Act
+    result = cut.diagnose(arg_time_step)
+
+    # Assert
+    assert result == None
+
+# class TestBrain(unittest.TestCase):
+
+#     def setUp(self):
+#         self.test_path = os.path.dirname(os.path.abspath(__file__))
+#         SC = Spacecraft(['TIME', 'A', 'B'], [[['SYNC', 'TIME']], [['NOOP']], [['NOOP']]])
+#         self.B = Brain(SC)
+
+#     def test_init_nonempty_brain(self):
+#         self.assertEqual(type(self.B.spacecraft_rep), Spacecraft)
+#         self.assertEqual(self.B.mission_status, '---')
+#         self.assertEqual(self.B.bayesian_status, ('---', -1.0))
+
+#     def test_reason(self):
+#         frame = [1, 1, 1]
+#         self.B.reason(frame)
+#         self.assertEqual(self.B.spacecraft_rep.get_current_data(), [1,1,1])
+#         self.assertEqual(self.B.mission_status, 'GREEN')
+
+#     def test_diagnose(self):
+#         return
         
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
