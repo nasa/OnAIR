9682e70ad8497f353a670a174dbb96277cb47f52
asgibso1 <alan.s.gibson@nasa.gov>
2023-01-10 13:59:33 -0500

    Wrote `__init__` tests
    Did not remove or comment earlier tests yet, trying a new approach
    Will remove them after the pytest tests are written

--- src/test/data_handling/test_time_synchronizer.py
+++ src/test/data_handling/test_time_synchronizer.py
@@ -2,7 +2,87 @@
 import os
 import unittest
 
+from mock import MagicMock
+import src.data_handling.time_synchronizer as time_synchronizer
 from src.data_handling.time_synchronizer import TimeSynchronizer
+
+# __init__ tests
+def test_init_does_not_set_instance_default_values_when_calls_to_init_sync_data_and_sort_data_do_not_raise_exceptions(mocker):
+    # Arrange
+    arg_headers = MagicMock()
+    arg_dataFrames = MagicMock()
+    arg_test_configs = MagicMock()
+
+    cut = TimeSynchronizer.__new__(TimeSynchronizer)
+
+    mocker.patch.object(cut, 'init_sync_data')
+    mocker.patch.object(cut, 'sort_data')
+
+    # Act
+    cut.__init__(arg_headers, arg_dataFrames, arg_test_configs)
+
+    # Assert
+    assert cut.init_sync_data.call_count == 1
+    assert cut.init_sync_data.call_args_list[0].args == (arg_headers, arg_test_configs)
+    assert cut.sort_data.call_count == 1
+    assert cut.sort_data.call_args_list[0].args == (arg_dataFrames, )
+    assert hasattr(cut, 'ordered_sources') == False
+    assert hasattr(cut, 'ordered_fused_headers') == False
+    assert hasattr(cut, 'ordered_fused_tests') == False
+    assert hasattr(cut, 'indices_to_remove') == False
+    assert hasattr(cut, 'offsets') == False
+    assert hasattr(cut, 'sim_data') == False
+
+def test_init_sets_instance_default_values_when_call_to_init_sync_data_raises_exception(mocker):
+    # Arrange
+    arg_headers = MagicMock()
+    arg_dataFrames = MagicMock()
+    arg_test_configs = MagicMock()
+
+    cut = TimeSynchronizer.__new__(TimeSynchronizer)
+
+    mocker.patch.object(cut, 'init_sync_data', side_effect=Exception())
+    mocker.patch.object(cut, 'sort_data')
+
+    # Act
+    cut.__init__(arg_headers, arg_dataFrames, arg_test_configs)
+
+    # Assert
+    assert cut.init_sync_data.call_count == 1
+    assert cut.init_sync_data.call_args_list[0].args == (arg_headers, arg_test_configs)
+    assert cut.sort_data.call_count == 0
+    assert cut.ordered_sources == []
+    assert cut.ordered_fused_headers == []
+    assert cut.ordered_fused_tests == []
+    assert cut.indices_to_remove == []
+    assert cut.offsets == {}
+    assert cut.sim_data == []
+
+def test_init_sets_instance_default_values_when_call_to_sort_data_raises_exception(mocker):
+    # Arrange
+    arg_headers = MagicMock()
+    arg_dataFrames = MagicMock()
+    arg_test_configs = MagicMock()
+
+    cut = TimeSynchronizer.__new__(TimeSynchronizer)
+
+    mocker.patch.object(cut, 'init_sync_data')
+    mocker.patch.object(cut, 'sort_data', side_effect=Exception())
+
+    # Act
+    cut.__init__(arg_headers, arg_dataFrames, arg_test_configs)
+
+    # Assert
+    assert cut.init_sync_data.call_count == 1
+    assert cut.init_sync_data.call_args_list[0].args == (arg_headers, arg_test_configs)
+    assert cut.sort_data.call_count == 1
+    assert cut.sort_data.call_args_list[0].args == (arg_dataFrames, )
+    assert cut.ordered_sources == []
+    assert cut.ordered_fused_headers == []
+    assert cut.ordered_fused_tests == []
+    assert cut.indices_to_remove == []
+    assert cut.offsets == {}
+    assert cut.sim_data == []
 
 class TestTimeSynchronizer(unittest.TestCase):
 
