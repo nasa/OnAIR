97402b5878212f1afc4a5c1215c35d3fcfa91161
Charles Zhao <charles.zhao-1@nasa.gov>
2023-02-10 15:25:46 -0500

    formatting, style, and naming updates

--- src/test/data_handling/parsers/test_forty_two_parser.py
+++ src/test/data_handling/parsers/test_forty_two_parser.py
@@ -269,7 +269,7 @@ def test_forty_two_parse_sim_data_with_more_than_one_data_pt(mocker):
     for i in range(num_data_pts):
         assert cut.parse_frame.call_args_list[i].args == (data_pts[i],)
 
-def test_forty_two_parse_headers_for_frame_with_data():
+def test_forty_two_parse_headers_returns_expected_value_when_given_frame_with_data():
     # Arrange
     num_lines = pytest.gen.randint(1, 10) # arbitrary, from 1 to 10
     fake_time = MagicMock()
@@ -280,6 +280,7 @@ def test_forty_two_parse_headers_for_frame_with_data():
     for fake_header in fake_headers:
         expected_result.append(str(fake_header))
         arg_frame += '\n' + str(fake_header) + ' = '
+        
     cut = FortyTwo.__new__(FortyTwo)
 
     # Act
@@ -289,19 +290,20 @@ def test_forty_two_parse_headers_for_frame_with_data():
     assert result == expected_result
 
 # tests for parse frame
-def test_forty_two_parse_frame_raises_error_because_of_frame_with_no_data():
+def test_forty_two_parse_frame_raises_error_when_given_frame_with_no_data():
     # Arrange
     cut = FortyTwo.__new__(FortyTwo)
-    
+    arg_frame = ''
+
     # Act
     with pytest.raises(IndexError) as e_info:
-        cut.parse_frame('')
+        cut.parse_frame(arg_frame)
 
     # Assert
     assert e_info.match('list index out of range')
 
 
-def test_forty_two_parse_frame_for_frame_with_data():
+def test_forty_two_parse_frame_returns_expected_value_when_given_frame_with_data():
     # Arrange
     num_lines = pytest.gen.randint(1, 10) # arbitrary, from 1 to 10
     
@@ -327,15 +329,16 @@ def test_forty_two_parse_frame_for_frame_with_data():
 def test_forty_two_parse_header_returns_list_with_a_single_empty_string_for_frame_with_no_data():
     # Arrange
     cut = FortyTwo.__new__(FortyTwo)
+    arg_frame = ''
     
     # Act
-    result = cut.parse_headers('')
+    result = cut.parse_headers(arg_frame)
 
     # Assert
     assert result == ['']
 
 # tests for parse config data
-def test_forty_two_parse_config_data_when_ss_breakdown_is_false_and_only_one_ss_assignment_returns_expected_result(mocker):
+def test_forty_two_parse_config_data_returns_expected_result_when_ss_breakdown_is_false_and_only_one_ss_assignment(mocker):
     # Arrange
     arg_config_file = MagicMock()
 
@@ -370,11 +373,11 @@ def test_forty_two_parse_config_data_when_ss_breakdown_is_false_and_only_one_ss_
     assert forty_two_parser.process_filepath.call_args_list[0].args == (arg_config_file,)
     assert forty_two_parser.process_filepath.call_args_list[1].args == (arg_config_file,)
 
-def test_forty_two_parse_config_data_if_ss_breakdown_is_false_and_number_of_subsystem_assignments_greater_than_one(mocker):
+def test_forty_two_parse_config_data_returns_expected_result_when_ss_breakdown_is_false_and_number_of_subsystem_assignments_greater_than_one(mocker):
     # Arrange
     arg_config_file = MagicMock()
-    fake_metadata_file_path = MagicMock()
 
+    fake_metadata_file_path = MagicMock()
     fake_filename = MagicMock()
     num_ss_assignments = pytest.gen.randint(2, 10) # arbitrary, from 2 to 10
     fake_subsystem_assignments = [MagicMock()] * num_ss_assignments
@@ -387,8 +390,9 @@ def test_forty_two_parse_config_data_if_ss_breakdown_is_false_and_number_of_subs
     forced_return_extract_configs = { 'subsystem_assignments' : {fake_filename:fake_subsystem_assignments},
                                         'test_assignments' : {fake_filename:fake_tests},
                                         'description_assignments' : {fake_filename:fake_descs}}
-    mocker.patch('src.data_handling.parsers.forty_two_parser.extract_configs', return_value=forced_return_extract_configs)
     forced_return_process_filepath = fake_filename
+    
+    mocker.patch('src.data_handling.parsers.forty_two_parser.extract_configs', return_value=forced_return_extract_configs)
     mocker.patch('src.data_handling.parsers.forty_two_parser.process_filepath', return_value=forced_return_process_filepath)
 
     expected_result = { 'subsystem_assignments' : {fake_filename:[['MISSION']] * num_ss_assignments},
@@ -405,11 +409,11 @@ def test_forty_two_parse_config_data_if_ss_breakdown_is_false_and_number_of_subs
     assert forty_two_parser.process_filepath.call_args_list[0].args == (arg_config_file,)
     assert forty_two_parser.process_filepath.call_args_list[1].args == (arg_config_file,)
 
-def test_forty_two_parse_config_data_if_ss_breakdown_is_true(mocker):
+def test_forty_two_parse_config_data_returns_return_value_of_extract_configs_when_ss_breakdown_is_true(mocker):
     # Arrange
     arg_config_file = MagicMock()
-    fake_metadata_file_path = MagicMock()
 
+    fake_metadata_file_path = MagicMock()
     fake_filename = MagicMock()
     fake_subsystem_assignments = MagicMock()
     fake_tests = MagicMock()
