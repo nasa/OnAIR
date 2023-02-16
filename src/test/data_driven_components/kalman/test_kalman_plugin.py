8bd2452e5068212969e518610b033e25e4e14295
Charles Zhao <charles.zhao-1@nasa.gov>
2023-02-16 16:34:54 -0500

    tested mandated functions (42% coverage)

--- src/test/data_driven_components/kalman/test_kalman_plugin.py
+++ src/test/data_driven_components/kalman/test_kalman_plugin.py
@@ -1,26 +1,34 @@
-""" Test Kalman Core Functionality """
+""" Test Kalman Plugin Functionality """
 
 import pytest
 from mock import MagicMock
-import src.data_driven_components.kalman.core as core
-from src.data_driven_components.kalman.core import AIPlugIn
+import src.data_driven_components.kalman.kalman_plugin as kalman
+from src.data_driven_components.kalman.kalman_plugin import Kalman
 
 import importlib
 
 # test init
-def test_AIPlugIn__init__initializes_variables_to_expected_values_when_given_all_args_except_window_size(mocker):
+def test_Kalman__init__initializes_variables_to_expected_values_when_given_all_args_except_window_size(mocker):
     # Arrange
     arg_name = MagicMock()
     arg_headers = MagicMock()
 
     fake_var = MagicMock()
-    class Fake_kalman():
-        def __init__(self):
+    class Fake_KalmanFilter():
+        def __init__(self, state_transition, process_noise, observation_model, observation_noise):
             self.test_var = fake_var
+            self.state_transition = state_transition
+            self.process_noise = process_noise
+            self.observation_model = observation_model
+            self.observation_noise = observation_noise
 
-    mocker.patch('src.data_driven_components.kalman.core.Kalman', Fake_kalman)
+    forced_diag_return_value = MagicMock()
+    forced_array_return_value = MagicMock()
+    mocker.patch('src.data_driven_components.kalman.kalman_plugin.simdkalman.KalmanFilter', Fake_KalmanFilter)
+    mocker.patch('src.data_driven_components.kalman.kalman_plugin.np.diag', return_value=forced_diag_return_value)
+    mocker.patch('src.data_driven_components.kalman.kalman_plugin.np.array', return_value=forced_array_return_value)
 
-    cut = AIPlugIn.__new__(AIPlugIn)
+    cut = Kalman.__new__(Kalman)
 
     # Act
     cut.__init__(arg_name, arg_headers)
@@ -30,22 +38,35 @@ def test_AIPlugIn__init__initializes_variables_to_expected_values_when_given_all
     assert cut.component_name == arg_name
     assert cut.headers == arg_headers
     assert cut.window_size == 3
-    assert cut.agent.test_var == fake_var
+    assert isinstance(cut.kf, Fake_KalmanFilter)
+    assert cut.kf.test_var == fake_var
+    assert cut.kf.state_transition == [[1,1],[0,1]]
+    assert cut.kf.process_noise == forced_diag_return_value
+    assert cut.kf.observation_model == forced_array_return_value
+    assert cut.kf.observation_noise == 1.0
   
-def test_AIPlugIn__init__initializes_variables_to_expected_values_when_given_all_args(mocker):
+def test_Kalman__init__initializes_variables_to_expected_values_when_given_all_args(mocker):
     # Arrange
     arg_name = MagicMock()
     arg_headers = MagicMock()
     arg_window_size = MagicMock()
 
     fake_var = MagicMock()
-    class Fake_kalman():
-        def __init__(self):
+    class Fake_KalmanFilter():
+        def __init__(self, state_transition, process_noise, observation_model, observation_noise):
             self.test_var = fake_var
+            self.state_transition = state_transition
+            self.process_noise = process_noise
+            self.observation_model = observation_model
+            self.observation_noise = observation_noise
 
-    mocker.patch('src.data_driven_components.kalman.core.Kalman', Fake_kalman)
+    forced_diag_return_value = MagicMock()
+    forced_array_return_value = MagicMock()
+    mocker.patch('src.data_driven_components.kalman.kalman_plugin.simdkalman.KalmanFilter', Fake_KalmanFilter)
+    mocker.patch('src.data_driven_components.kalman.kalman_plugin.np.diag', return_value=forced_diag_return_value)
+    mocker.patch('src.data_driven_components.kalman.kalman_plugin.np.array', return_value=forced_array_return_value)
 
-    cut = AIPlugIn.__new__(AIPlugIn)
+    cut = Kalman.__new__(Kalman)
 
     # Act
     cut.__init__(arg_name, arg_headers, arg_window_size)
@@ -55,12 +76,17 @@ def test_AIPlugIn__init__initializes_variables_to_expected_values_when_given_all
     assert cut.component_name == arg_name
     assert cut.headers == arg_headers
     assert cut.window_size == arg_window_size
-    assert cut.agent.test_var == fake_var
+    assert isinstance(cut.kf, Fake_KalmanFilter)
+    assert cut.kf.test_var == fake_var
+    assert cut.kf.state_transition == [[1,1],[0,1]]
+    assert cut.kf.process_noise == forced_diag_return_value
+    assert cut.kf.observation_model == forced_array_return_value
+    assert cut.kf.observation_noise == 1.0
     
 # test apiori training
-def test_AIPlugIn_apiori_training_returns_none():
+def test_Kalman_apiori_training_returns_none():
     # Arrange
-    cut = AIPlugIn.__new__(AIPlugIn)
+    cut = Kalman.__new__(Kalman)
 
     # Act
     result = cut.apriori_training()  
@@ -69,12 +95,12 @@ def test_AIPlugIn_apiori_training_returns_none():
     assert result == None
 
 # test update
-def test_AIPlugIn_update_does_not_mutate_frames_attribute_when_arg_frame_is_empty():
+def test_Kalman_update_does_not_mutate_frames_attribute_when_arg_frame_is_empty():
     # Arrange
     fake_frames = MagicMock()
     arg_frame = []
 
-    cut = AIPlugIn.__new__(AIPlugIn)
+    cut = Kalman.__new__(Kalman)
     cut.frames = fake_frames
 
     # Act
@@ -83,13 +109,13 @@ def test_AIPlugIn_update_does_not_mutate_frames_attribute_when_arg_frame_is_empt
     # Assert
     assert cut.frames == fake_frames
 
-def test_AIPlugIn_update_mutates_frames_attribute_as_expected_when_frames_is_empty_and_arg_frame_is_not_empty():
+def test_Kalman_update_mutates_frames_attribute_as_expected_when_frames_is_empty_and_arg_frame_is_not_empty():
     # Arrange
     fake_frames = []
     len_arg_frame = pytest.gen.randint(1, 10) # arbitrary, random integer from 1 to 10
     arg_frame = [MagicMock()] * len_arg_frame
 
-    cut = AIPlugIn.__new__(AIPlugIn)
+    cut = Kalman.__new__(Kalman)
     cut.frames = fake_frames
 
     expected_result = []
@@ -102,7 +128,7 @@ def test_AIPlugIn_update_mutates_frames_attribute_as_expected_when_frames_is_emp
     # Assert
     assert cut.frames == expected_result
 
-def test_AIPlugIn_update_mutates_frames_attribute_as_expected_when_both_frames_and_arg_frame_are_not_empty_and_len_arg_frame_greater_than_len_frames():
+def test_Kalman_update_mutates_frames_attribute_as_expected_when_both_frames_and_arg_frame_are_not_empty_and_len_arg_frame_greater_than_len_frames():
     # Arrange
     len_fake_frames = pytest.gen.randint(1, 5) # arbitrary, random int from 1 to 5
     fake_frames = [[MagicMock()]] * len_fake_frames
@@ -111,7 +137,7 @@ def test_AIPlugIn_update_mutates_frames_attribute_as_expected_when_both_frames_a
     len_arg_frame = pytest.gen.randint(6, 10) # arbitrary int greater than max len of fake_frames, from 6 to 10
     arg_frame = [MagicMock()] * len_arg_frame
 
-    cut = AIPlugIn.__new__(AIPlugIn)
+    cut = Kalman.__new__(Kalman)
     cut.frames = fake_frames
     cut.window_size = fake_window_size
 
@@ -132,7 +158,7 @@ def test_AIPlugIn_update_mutates_frames_attribute_as_expected_when_both_frames_a
     # Assert
     assert cut.frames == expected_result
 
-def test_AIPlugIn_update_mutates_frames_attribute_as_expected_when_both_frames_and_arg_frame_are_not_empty_and_len_arg_frame_less_than_len_frames():
+def test_Kalman_update_mutates_frames_attribute_as_expected_when_both_frames_and_arg_frame_are_not_empty_and_len_arg_frame_less_than_len_frames():
     # Arrange
     len_fake_frames = pytest.gen.randint(6, 10) # arbitrary int greater than max len of arg_frame, from 6 to 10
     fake_frames = [[MagicMock()]] * len_fake_frames
@@ -141,7 +167,7 @@ def test_AIPlugIn_update_mutates_frames_attribute_as_expected_when_both_frames_a
     len_arg_frame = pytest.gen.randint(1, 5) # arbitrary, random int from 1 to 5
     arg_frame = [MagicMock()] * len_arg_frame
 
-    cut = AIPlugIn.__new__(AIPlugIn)
+    cut = Kalman.__new__(Kalman)
     cut.frames = fake_frames
     cut.window_size = fake_window_size
 
@@ -158,7 +184,7 @@ def test_AIPlugIn_update_mutates_frames_attribute_as_expected_when_both_frames_a
     assert cut.frames == expected_result
 
 
-def test_AIPlugIn_update_pops_first_index_of_frames_data_points_when_window_size_is_exceeded():
+def test_Kalman_update_pops_first_index_of_frames_data_points_when_window_size_is_exceeded():
     # Arrange
     len_fake_frames = pytest.gen.randint(6, 10) # arbitrary int greater than max len of arg_frame, from 6 to 10
                                                 # choosing to keep len of fake_frames greater than arg_frame in order to guarantee 'popping'
@@ -168,7 +194,7 @@ def test_AIPlugIn_update_pops_first_index_of_frames_data_points_when_window_size
     len_arg_frame = pytest.gen.randint(1, 5) # arbitrary, random int from 1 to 5
     arg_frame = [MagicMock()] * len_arg_frame
 
-    cut = AIPlugIn.__new__(AIPlugIn)
+    cut = Kalman.__new__(Kalman)
     cut.frames = fake_frames
     cut.window_size = fake_window_size
 
@@ -185,19 +211,17 @@ def test_AIPlugIn_update_pops_first_index_of_frames_data_points_when_window_size
     assert cut.frames == expected_result
 
 # test render diagnosis
-def test_AIPlugIn_render_diagnosis_returns_value_returned_by_agent_frame_diagnose_function(mocker):
+def test_Kalman_render_diagnosis_returns_value_returned_by_agent_frame_diagnose_function(mocker):
     # Arrange
-    fake_agent = MagicMock()
     fake_frames = MagicMock()
     fake_headers = MagicMock()
     forced_frame_diagnose_return = MagicMock()
-
-    mocker.patch.object(fake_agent, 'frame_diagnose', return_value=forced_frame_diagnose_return)
-
-    cut = AIPlugIn.__new__(AIPlugIn)
-    cut.agent = fake_agent
+    
+    cut = Kalman.__new__(Kalman)
     cut.frames = fake_frames
     cut.headers = fake_headers
+
+    mocker.patch.object(cut, 'frame_diagnosis', return_value=forced_frame_diagnose_return)
     
     # Act
     result = cut.render_diagnosis()
