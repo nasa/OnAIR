080f953ee9c421ba325074435904f45bd2f2c18e
James Staley <james.staley@nasa.gov>
2023-02-16 15:47:14 -0500

    Removed top level generic_intelligence_construct, make AIPlugIn mostly abstract, updated tests, add skeleton kalman_filter.py to give clean AIPlugIn instantiation example

--- src/data_driven_components/generic_component/core.py
+++ src/data_driven_components/generic_component/core.py
@@ -1,29 +1,42 @@
-
-from src.data_driven_components.generic_intelligence_construct import GenericIntelligenceConstruct
-
+from abc import ABC, abstractmethod
 """This object serves as a proxy for all plug-ins.
    Therefore, the AIPlugIn object is meant to induce 
    standards and structures of compliance for user-created 
    and/or imported plug-ins/libraries
 """
-class AIPlugIn(GenericIntelligenceConstruct):
-
+class AIPlugIn(ABC):
     def __init__(self, _name, _headers):
+        """
+        Superclass for data driven components: VAE, PPO, etc. Allows for easier modularity.
+        """
         assert(len(_headers)>0)
         self.component_name = _name
         self.headers = _headers
 
-    def apriori_training(self, batch_data): 
+    @abstractmethod
+    def apriori_training(self, batch_data=[]):
+        """
+        Given data, system should learn any priors necessary for realtime diagnosis.
+        """
+        # I dont know yet whether we should allow empty frames from updates 
         # The batch data format could change 
         # depending on how the tutorial fleshes out (too early to tell) 
         # There will be no return from this function (user can pull training)
         # data from the construct itself) 
         # I dont know yet whether we should allow empty batch data
         pass
-
+        
+    @abstractmethod
     def update(self, frame=[]):
-        # I dont know yet whether we should allow empty frames from updates 
-        assert(len(frame) == len(self.headers))
+        """
+        Given streamed data point, system should update internally
+        """
+        pass
 
+    @abstractmethod
     def render_diagnosis(self):
-        return []
+        """
+        System should return its diagnosis
+        """
+        pass
+
