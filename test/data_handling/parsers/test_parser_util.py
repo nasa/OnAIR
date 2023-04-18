8dc7e6f1d266fd73a3eeb43f721da3f5322d75ae
Charles Zhao <charles.zhao-1@nasa.gov>
2023-04-18 11:51:41 -0400

    fixed testing mistake. Added test for bug fix

--- test/data_handling/parsers/test_parser_util.py
+++ test/data_handling/parsers/test_parser_util.py
@@ -67,10 +67,11 @@ def test_parser_util_extract_configs_returns_expected_dicts_dict_when_configs_le
     mocker.patch('data_handling.parsers.parser_util.parseTlmConfJson', return_value=forced_return_parse_tlm)
     mocker.patch('data_handling.parsers.parser_util.str2lst', return_value=forced_return_str2lst)
 
+    expected_ss_assigns = [[fake_ss_assign] for fake_ss_assign in fake_subsystem_assignments]
     expected_result = {}
-    expected_result['subsystem_assignments'] = fake_subsystem_assignments
+    expected_result['subsystem_assignments'] = expected_ss_assigns
     expected_result['test_assignments'] = [[forced_return_str2lst]]
-    expected_result['description_assignments'] = fake_descs
+    expected_result['description_assignments'] = fake_descs.copy()
 
     # Act
     result = parser_util.extract_configs(arg_configFilePath, arg_configFile, arg_csv)
@@ -102,10 +103,11 @@ def test_parser_util_extract_configs_returns_expected_dicts_dict_when_len_config
     mocker.patch('data_handling.parsers.parser_util.parseTlmConfJson', return_value=forced_return_parse_tlm)
     mocker.patch('data_handling.parsers.parser_util.str2lst', return_value=forced_return_str2lst)
 
+    expected_ss_assigns = [[fake_ss_assign] for fake_ss_assign in fake_subsystem_assignments]
     expected_result = {}
-    expected_result['subsystem_assignments'] = fake_subsystem_assignments
+    expected_result['subsystem_assignments'] = expected_ss_assigns
     expected_result['test_assignments'] = [[forced_return_str2lst]] * len_configs
-    expected_result['description_assignments'] = fake_descs
+    expected_result['description_assignments'] = fake_descs.copy()
 
     # Act
     result = parser_util.extract_configs(arg_configFilePath, arg_configFile, arg_csv)
@@ -140,10 +142,11 @@ def test_parser_util_extract_configs_returns_expected_dicts_dict_when_len_config
     mocker.patch('data_handling.parsers.parser_util.parseTlmConfJson', return_value=forced_return_parse_tlm)
     mocker.patch('data_handling.parsers.parser_util.str2lst', return_value=forced_return_str2lst)
 
+    expected_ss_assigns = [[fake_ss_assign] for fake_ss_assign in fake_subsystem_assignments]
     expected_result = {}
-    expected_result['subsystem_assignments'] = fake_subsystem_assignments
+    expected_result['subsystem_assignments'] = expected_ss_assigns
     expected_result['test_assignments'] = [[forced_return_str2lst]] * (len_configs - num_noops) + [noop_test_assign] * num_noops
-    expected_result['description_assignments'] = fake_descs
+    expected_result['description_assignments'] = fake_descs.copy()
 
     # Act
     result = parser_util.extract_configs(arg_configFilePath, arg_configFile, arg_csv)
@@ -179,16 +182,17 @@ def test_parser_util_extract_configs_returns_expected_dicts_dict_when_len_config
     mocker.patch('data_handling.parsers.parser_util.parseTlmConfJson', return_value=forced_return_parse_tlm)
     mocker.patch('data_handling.parsers.parser_util.str2lst', return_value=forced_return_str2lst)
 
+    expected_ss_assigns = [[fake_ss_assign] for fake_ss_assign in fake_subsystem_assignments]
     expected_result = {}
-    expected_result['subsystem_assignments'] = fake_subsystem_assignments
-    expected_result['test_assignments'] = fake_tests
-    expected_result['description_assignments'] = fake_descs
+    expected_result['subsystem_assignments'] = expected_ss_assigns
+    expected_result['test_assignments'] = fake_tests.copy()
+    expected_result['description_assignments'] = fake_descs.copy()
 
     expected_str2lst_args = []
 
     for i in range(len_configs):
         if len(fake_tests[i]) > 1:
-            expected_result['test_assignments'][i] = [fake_tests[i][0]] + forced_return_str2lst
+            expected_result['test_assignments'][i] = [[fake_tests[i][0]] + forced_return_str2lst]
             expected_str2lst_args.append(fake_tests[i][1])
         else:
             expected_result['test_assignments'][i] = [forced_return_str2lst]
@@ -230,10 +234,11 @@ def test_parser_util_extract_configs_returns_expected_dicts_dict_when_configFile
     mocker.patch('data_handling.parsers.parser_util.parseTlmConfJson', return_value=forced_return_parse_tlm)
     mocker.patch('data_handling.parsers.parser_util.str2lst', return_value=forced_return_str2lst)
 
+    expected_ss_assigns = [[fake_ss_assign] for fake_ss_assign in fake_subsystem_assignments]
     expected_result = {}
-    expected_result['subsystem_assignments'] = fake_subsystem_assignments
+    expected_result['subsystem_assignments'] = expected_ss_assigns
     expected_result['test_assignments'] = [[forced_return_str2lst]] * len_configs
-    expected_result['description_assignments'] = fake_descs
+    expected_result['description_assignments'] = fake_descs.copy()
 
     # Act
     result = parser_util.extract_configs(arg_configFilePath, arg_configFile, arg_csv)
@@ -271,10 +276,65 @@ def test_parser_util_extract_configs_returns_expected_dicts_dict_when_configFile
     mocker.patch('data_handling.parsers.parser_util.parseTlmConfJson', return_value=forced_return_parse_tlm)
     mocker.patch('data_handling.parsers.parser_util.str2lst', return_value=forced_return_str2lst)
 
+    expected_ss_assigns = [[fake_ss_assign] for fake_ss_assign in fake_subsystem_assignments]
     expected_result = {}
-    expected_result['subsystem_assignments'] = fake_subsystem_assignments
-    expected_result['test_assignments'] = fake_tests
-    expected_result['description_assignments'] = fake_descs
+    expected_result['subsystem_assignments'] = expected_ss_assigns
+    expected_result['test_assignments'] = [[forced_return_str2lst]] * len_configs
+    expected_result['description_assignments'] = fake_descs.copy()
+
+    # Act
+    result = parser_util.extract_configs(arg_configFilePath, arg_configFile, arg_csv)
+
+    # Assert
+    assert parser_util.parseTlmConfJson.call_count == 1
+    assert parser_util.parseTlmConfJson.call_args_list[0].args == (arg_configFilePath + arg_configFile, )
+    assert parser_util.str2lst.call_count == len_configs
+    for i in range(len_configs):
+        assert parser_util.str2lst.call_args_list[i].args == (fake_tests_copy[i][0], )
+    assert result == expected_result
+
+def test_parser_util_extract_configs_returns_expected_dicts_dict_when_configFiles_has_many_cFile_and_len_configs_greater_than_one_and_subsystem_NONE_exists(mocker):
+    # Arrange
+    arg_configFilePath = MagicMock()
+    arg_configFile = MagicMock()
+    arg_csv = MagicMock()
+
+    len_configs = pytest.gen.randint(2, 10) # arbitrary, from 2 to 10 (0 and 1 have own tests)
+    fake_subsystem_assignments = []
+    fake_tests = []
+    fake_tests_copy = []
+    fake_descs = []
+
+    expected_subsystem_assignments = []
+    for i in range(len_configs):
+        if pytest.gen.randint(0, 1) == 1:
+            fake_ss_assign = MagicMock()
+            fake_subsystem_assignments.append(fake_ss_assign)
+            expected_subsystem_assignments.append([fake_ss_assign])
+        else:
+            fake_subsystem_assignments.append('NONE')
+            expected_subsystem_assignments.append([])
+        fake_test_assign = [MagicMock()]
+        fake_tests.append(fake_test_assign)
+        fake_tests_copy.append(fake_test_assign)
+        fake_descs.append(MagicMock())
+
+    rand_index = pytest.gen.randint(0, len_configs-1) # arbitrary index in fake_subsystem_assignments
+    fake_subsystem_assignments[rand_index] = 'NONE'
+    expected_subsystem_assignments[rand_index] = []
+
+    forced_return_parse_tlm = {'subsystem_assignments' : fake_subsystem_assignments,
+                                'test_assignments' : fake_tests,
+                                'description_assignments' : fake_descs}
+    forced_return_str2lst = [MagicMock()]
+
+    mocker.patch('data_handling.parsers.parser_util.parseTlmConfJson', return_value=forced_return_parse_tlm)
+    mocker.patch('data_handling.parsers.parser_util.str2lst', return_value=forced_return_str2lst)
+
+    expected_result = {}
+    expected_result['subsystem_assignments'] = expected_subsystem_assignments
+    expected_result['test_assignments'] = [[forced_return_str2lst]] * len_configs
+    expected_result['description_assignments'] = fake_descs.copy()
 
     # Act
     result = parser_util.extract_configs(arg_configFilePath, arg_configFile, arg_csv)
