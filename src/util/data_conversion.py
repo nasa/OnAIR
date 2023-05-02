56773447ee9d3ecd2c299ba563c8c7650bb73b56
Charles Zhao <charles.zhao-1@nasa.gov>
2023-05-02 12:19:42 -0400

    replaced if by specifying exception type

--- src/util/data_conversion.py
+++ src/util/data_conversion.py
@@ -21,22 +21,19 @@ classes = {'RED' : 0,
 def floatify_input(_input, remove_str=False):
     floatified = []
     for i in _input:
-        if type(i) is str:
+        try:
+            x = float(i)
+            floatified.append(x)
+        except ValueError:
             try:
-                x = float(i)
-                floatified.append(x)
+                x = i.replace('-', '').replace(':', '').replace('.', '')
+                floatified.append(float(x))
             except:
-                try:
-                    x = i.replace('-', '').replace(':', '').replace('.', '')
-                    floatified.append(float(x))
-                except:
-                    if remove_str == False:
-                        floatified.append(0.0)
-                    else:
-                        continue
+                if remove_str == False:
+                    floatified.append(0.0)
+                else:
                     continue
-        else:
-            floatified.append(float(i))
+                continue
     return floatified
 
 def status_to_oneHot(status):
