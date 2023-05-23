025471710036788459e73f0fcca867cd577236ca
Marshall, James C 459937851 <james.marshall-1@nasa.gov>
2023-05-23 11:14:16 -0400

    Remove comment

--- src/util/cleanup.py
+++ src/util/cleanup.py
@@ -5,7 +5,6 @@ Utility file to clean and remove unwanted files post-run, or setup folders pre-r
 
 import os
 
-# TODO: These two lines used in one place do not warrant their own function
 def setup_folders(results_path):
     if not os.path.isdir(results_path):
         os.mkdir(results_path)
