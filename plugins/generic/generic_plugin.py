e8ca3518808ae112e4d758ad516d5a6d0007bc66
cfirth-nasa <connor.c.firth@nasa.gov>
2023-09-11 13:09:49 -0400

    Add functionality and unit tests for external plugins

--- plugins/generic/generic_plugin.py
+++ plugins/generic/generic_plugin.py
@@ -8,24 +8,16 @@
 # See "NOSA GSC-19165-1 OnAIR.pdf"
 
 import numpy as np
-from onair.src.data_driven_components.generic_component.core import AIPlugIn
+from onair.src.data_driven_components.ai_plugin_abstract.core import AIPlugIn
 
 class Plugin(AIPlugIn):
-    def __init__(self, _name, _headers):
-        """
-        Superclass for data driven components: VAE, PPO, etc. Allows for easier modularity.
-        """
-        assert(len(_headers)>0)
-        self.component_name = _name
-        self.headers = _headers
-
-    def apriori_training(self, batch_data=[]):
+    def apriori_training(self):
         """
         Given data, system should learn any priors necessary for realtime diagnosis.
         """
         pass
             
-    def update(self, frame=[]):
+    def update(self):
         """
         Given streamed data point, system should update internally
         """
---
+++ test/plugins/generic/test_generic_plugin.py
@@ -0,0 +1,46 @@
+# GSC-19165-1, "The On-Board Artificial Intelligence Research (OnAIR) Platform"
+#
+# Copyright © 2023 United States Government as represented by the Administrator of
+# the National Aeronautics and Space Administration. No copyright is claimed in the
+# United States under Title 17, U.S. Code. All Other Rights Reserved.
+#
+# Licensed under the NASA Open Source Agreement version 1.3
+# See "NOSA GSC-19165-1 OnAIR.pdf"
+
+""" Test Kalman Plugin Functionality """
+import pytest
+from mock import MagicMock
+
+from plugins.generic import generic_plugin
+from plugins.generic import Plugin
+
+# test apriori training
+def test_apriori_training_does_nothing():
+    # Arrange
+    cut = Plugin.__new__(Plugin)
+
+    # Act
+    result = cut.apriori_training()
+
+    # Assert
+    assert result == None
+
+def test_update_does_nothing():
+    # Arrange
+    cut = Plugin.__new__(Plugin)
+
+    # Act
+    result = cut.update()
+
+    # Assert
+    assert result == None
+
+def test_render_reasoning_does_nothing():
+    # Arrange
+    cut = Plugin.__new__(Plugin)
+    
+    # Act
+    result = cut.render_reasoning()
+
+    # Assert
+    assert result == None
