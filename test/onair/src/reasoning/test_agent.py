e6b1368a7c8f219560bbe357aad000001b3706f1
Alan Gibson <alan.s.gibson@nasa.gov>
2023-10-24 14:21:55 -0400

    Agent updates
    Agent reason function
      was not properly sending curr_data to learning systems
      now updating data and using curr_data
      no more sending arguments to render_resoning (was not used in func)

--- test/onair/src/reasoning/test_agent.py
+++ test/onair/src/reasoning/test_agent.py
@@ -62,49 +62,82 @@ def test_Agent__init__sets_vehicle_rep_to_given_vehicle_and_learning_systems_and
     assert arg_vehicle.get_bayesian_status.call_args_list[0].args == ()
     assert cut.bayesian_status == fake_bayesian_status
 
-# reason tests
-def test_Agent_reason_updates_vehicle_rep_with_given_frame_and_sets_new_vehicle_mission_status_and_updates_learning_systems_with_given_frame_and_new_mission_status(mocker):
+# render_resoning tests
+def test_Agent_render_reasoning_returns_call_to_complex_reasoning_systems_render_reasoning(mocker):
     # Arrange
-    arg_frame = MagicMock()
-    arg_vehicle = MagicMock()
-    arg_learners_plugin_dict = MagicMock()
-    arg_planners_plugin_dict = MagicMock()
-    arg_complex_plugin_dict = MagicMock()
+    expected_result = MagicMock()
+    fake_complex_resoning_systems = MagicMock()
 
-    # Mock and patch
-    fake_mission_status = MagicMock()
-    fake_headers = MagicMock()
-    fake_headers.__len__.return_value = 1 # Fake a header
-    fake_learning_systems = MagicMock()
-    fake_planning_systems = MagicMock()
-    fake_complex_systems = MagicMock()
-    fake_state_information = MagicMock()
+    cut = Agent.__new__(Agent)
+    cut.complex_reasoning_systems = fake_complex_resoning_systems
 
-    mocker.patch.object(arg_vehicle, 'get_headers', return_value=fake_headers)
-    mocker.patch.object(arg_vehicle, 'update')
-    mocker.patch.object(arg_vehicle, 'get_status', return_value=fake_mission_status)
-    mocker.patch.object(arg_vehicle, 'get_state_information', return_value=fake_state_information)
-    mocker.patch(agent.__name__ + '.LearnersInterface', return_value=fake_learning_systems)
-    mocker.patch(agent.__name__ + '.PlannersInterface', return_value=fake_planning_systems)
-    mocker.patch(agent.__name__ + '.ComplexReasoningInterface', return_value=fake_complex_systems)
+    mocker.patch.object(fake_complex_resoning_systems, 'render_reasoning', return_value=expected_result)
 
     # Act
-    cut = Agent.__new__(Agent)
-    cut.__init__(arg_vehicle, arg_learners_plugin_dict, arg_planners_plugin_dict, arg_complex_plugin_dict)
-    cut.reason(arg_frame)
+    result = cut.render_reasoning()
 
     # Assert
-    assert cut.vehicle_rep == arg_vehicle
-    assert arg_vehicle.get_headers.call_count == 3
-    assert cut.vehicle_rep.update.call_args_list[0].args == (arg_frame, ) 
-    assert cut.vehicle_rep.get_status.call_count == 1  
-    assert cut.vehicle_rep.get_status.call_args_list[0].args == () 
-    assert cut.learning_systems == fake_learning_systems
-    assert cut.learning_systems.update.call_count == 1  
-    assert cut.learning_systems.update.call_args_list[0].args == (arg_frame, fake_state_information) 
-    assert cut.planning_systems == fake_planning_systems
-    assert cut.planning_systems.update.call_count == 1 
-    assert cut.planning_systems.update.call_args_list[0].args == (fake_state_information, ) 
+    assert result == expected_result
+
+# reason tests
+def test_Agent_reason_updates_vehicle_rep_with_given_frame_and_updates_learning_systems_with_vehicle_curr_data_and_new_mission_status(mocker):
+    # Arrange
+    arg_frame = MagicMock()
+    fake_vehicle_rep = MagicMock()
+    fake_vehicle_rep.curr_data = MagicMock()
+
+    # Mock and patch
+    fake_status = MagicMock()
+    fake_PDDL_state = MagicMock()
+    fake_state = MagicMock()
+    fake_learning_systems = MagicMock()
+    fake_planning_systems = MagicMock()
+    fake_complex_reasoning_systems = MagicMock()
+    fake_learning_systems_reasoning = MagicMock()
+    fake_planning_systems_reasoning = MagicMock()
+    expected_aggregate_high_level_info = {'vehicle_rep': fake_state, 
+                                          'learning_systems':fake_learning_systems_reasoning,
+                                          'planning_systems':fake_planning_systems_reasoning}
+    expected_result = MagicMock()
+
+    cut = Agent.__new__(Agent)
+    cut.vehicle_rep = fake_vehicle_rep
+    cut.learning_systems = fake_learning_systems
+    cut.planning_systems = fake_planning_systems
+    cut.complex_reasoning_systems = fake_complex_reasoning_systems
+
+    mock_manager = mocker.MagicMock()
+
+    mock_manager.attach_mock(mocker.patch.object(fake_vehicle_rep, 'update'), 'cut.vehicle_rep.update')
+    mock_manager.attach_mock(mocker.patch.object(fake_learning_systems, 'update'), 'cut.learning_systems.update')
+    mock_manager.attach_mock(mocker.patch.object(fake_planning_systems, 'update'), 'cut.planning_systems.update')
+    mock_manager.attach_mock(mocker.patch.object(fake_complex_reasoning_systems, 'update'), 'cut.complex_reasoning_systems.update')
+    mock_manager.attach_mock(mocker.patch.object(cut, 'render_reasoning', return_value=expected_result), 'cut.render_reasoning')
+
+    mocker.patch.object(fake_vehicle_rep, 'get_state_information', side_effect=[fake_status, fake_PDDL_state, fake_state])
+    mocker.patch.object(fake_learning_systems, 'render_reasoning', return_value=fake_learning_systems_reasoning)
+    mocker.patch.object(fake_planning_systems, 'render_reasoning', return_value=fake_planning_systems_reasoning)
+
+    # Act
+    result = cut.reason(arg_frame)
+
+    # Assert
+    result = expected_result
+    mock_manager.assert_has_calls([
+        mocker.call.cut.vehicle_rep.update(arg_frame),
+        mocker.call.cut.learning_systems.update(fake_vehicle_rep.curr_data, fake_status),
+        mocker.call.cut.planning_systems.update(fake_PDDL_state),
+        mocker.call.cut.complex_reasoning_systems.update(expected_aggregate_high_level_info),
+    ], any_order=False)
+    assert cut.vehicle_rep.get_state_information.call_count == 3
+    assert cut.vehicle_rep.get_state_information.call_args_list[0].args == ()
+    assert cut.vehicle_rep.get_state_information.call_args_list[1].args == ()
+    assert cut.vehicle_rep.get_state_information.call_args_list[2].args == ()
+    assert cut.learning_systems.render_reasoning.call_count == 1
+    assert cut.learning_systems.render_reasoning.call_args_list[0].args == ()
+    assert cut.planning_systems.render_reasoning.call_count == 1
+    assert cut.planning_systems.render_reasoning.call_args_list[0].args == ()
+
      
 # diagnose tests
 def test_Agent_diagnose_returns_empty_Dict():
