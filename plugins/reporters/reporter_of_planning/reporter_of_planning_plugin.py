4611e517c7d29f5a9c6a31b696529524dedfd279
Alan Gibson <alan.s.gibson@nasa.gov>
2023-11-29 12:43:24 -0500

    Adding reporters for each plugin type
    Reporters will state who they are and what method is running
      also outputs the arguments, their types, and values
    Added reporter_config.ini that uses one of each kind in the config dicts

---
+++ plugins/reporters/reporter_of_planning/reporter_of_planning_plugin.py
@@ -0,0 +1,26 @@
+# GSC-19165-1, "The On-Board Artificial Intelligence Research (OnAIR) Platform"
+#
+# Copyright © 2023 United States Government as represented by the Administrator of
+# the National Aeronautics and Space Administration. No copyright is claimed in the
+# United States under Title 17, U.S. Code. All Other Rights Reserved.
+#
+# Licensed under the NASA Open Source Agreement version 1.3
+# See "NOSA GSC-19165-1 OnAIR.pdf"
+
+from onair.src.ai_components.ai_plugin_abstract.core import AIPlugIn
+
+class Plugin(AIPlugIn):
+    def update(self,low_level_data=[], high_level_data={}):
+        """
+        Given streamed data point, system should update internally
+        """
+        print(f"  PLANNING: UPDATE")
+        print(f"          :   low_level_data {low_level_data.__class__} = '{low_level_data}'")
+        print(f"          :   high_level_data {high_level_data.__class__} = '{high_level_data}'")
+
+    def render_reasoning(self):
+        """
+        System should return its diagnosis
+        """
+        print("  PLANNING: REASONING")
+        print("          :   No args")
