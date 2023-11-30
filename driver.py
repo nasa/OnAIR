bb06d54d93748ed7f548ffead59c47532e00b908
Alan Gibson <alan.s.gibson@nasa.gov>
2023-11-30 14:11:44 -0500

    Added -k arg to driver.py
    Allows running only specific tests matching EXPRESSION after -k
    Additionally update driver.py to be in line with PEP standards
    NOTE: this changed Copyright statements line distribution but not text

--- driver.py
+++ driver.py
@@ -1,8 +1,8 @@
 # GSC-19165-1, "The On-Board Artificial Intelligence Research (OnAIR) Platform"
 #
-# Copyright © 2023 United States Government as represented by the Administrator of
-# the National Aeronautics and Space Administration. No copyright is claimed in the
-# United States under Title 17, U.S. Code. All Other Rights Reserved.
+# Copyright © 2023 United States Government as represented by the Administrator
+# of the National Aeronautics and Space Administration. No copyright is claimed
+# in the United States under Title 17, U.S. Code. All Other Rights Reserved.
 #
 # Licensed under the NASA Open Source Agreement version 1.3
 # See "NOSA GSC-19165-1 OnAIR.pdf"
@@ -11,14 +11,11 @@
 Driver
 Source of the main function for the OnAIR repo
 """
-# coverage started early to see all lines in all files (def and imports were being missed with programmatic runs)
-
-
-
 import os
 import sys
 import argparse
 from datetime import datetime
+
 
 def main():
     """
@@ -31,26 +28,42 @@ def main():
     """
 
     arg_parser = argparse.ArgumentParser(description='')
-    arg_parser.add_argument('configfile', nargs='?', default='./onair/config/default_config.ini', help='Config file to be used')
-    arg_parser.add_argument('--save', '-s', action='store_true', help='Should log files be saved?')
-    arg_parser.add_argument('--save_name', '--name', '-n', help='Name of saved log files')
-    arg_parser.add_argument('--mute', '-m', action='store_true', help='Mute all non-error output')
+    arg_parser.add_argument('configfile', nargs='?',
+                            default='./onair/config/default_config.ini',
+                            help='Config file to be used')
+    arg_parser.add_argument('--save', '-s', action='store_true',
+                            help='Should log files be saved?')
+    arg_parser.add_argument('--save_name', '--name', '-n',
+                            help='Name of saved log files')
+    arg_parser.add_argument('--mute', '-m', action='store_true',
+                            help='Mute all non-error output')
 
     """
     Testing specific arguments
     """
-    arg_parser.add_argument('--test', '-t', action='store_true', help='Run tests')
-    arg_parser.add_argument('--verbose', '-v', action='count', default=0, help="Increase verbosity in tests")
-    arg_parser.add_argument('--conftest-seed', action='store', type=int, default=None, help="Set the random seed for test values")
-    arg_parser.add_argument('--randomly-seed', action='store', type=int, default=None, help="Set the random seed for test run order")
+    arg_parser.add_argument('--test', '-t', action='store_true',
+                            help='Run tests')
+    arg_parser.add_argument('--verbose', '-v', action='count', default=0,
+                            help="Increase verbosity in tests")
+    arg_parser.add_argument('-k', action='store', dest='keyword', default="",
+                            metavar='EXPRESSION',
+                            help="Pass thru for pytest's -k option. Runs only"
+                                 " tests with names that match EXPRESSION.")
+    arg_parser.add_argument('--conftest-seed', action='store',
+                            type=int, default=None,
+                            help="Set the random seed for test values")
+    arg_parser.add_argument('--randomly-seed', action='store',
+                            type=int, default=None,
+                            help="Set the random seed for test run order")
     args = arg_parser.parse_args()
 
     """
-    In test mode, covergage must start before imports from onair, otherwise lines are missed.
+    In test mode, covergage must start before imports from onair,
+    otherwise lines are missed.
     """
     if args.test:
         import coverage
-        cov = coverage.Coverage(source=['onair','plugins'], branch=True)
+        cov = coverage.Coverage(source=['onair', 'plugins'], branch=True)
         cov.start()
 
     """
@@ -69,39 +82,50 @@ def main():
         import pytest
         test_directory_name = "test"
         pytest_args = [test_directory_name]
+
         pytest_args.extend(['-v'] * args.verbose)
         if args.conftest_seed:
             pytest_args.extend([f"--conftest-seed={args.conftest_seed}"])
         if args.randomly_seed:
             pytest_args.extend([f"--randomly-seed={args.randomly_seed}"])
+        pytest_args.extend([f"-k {args.keyword}"])
+
         pytest.main(pytest_args)
         cov.stop()
         cov.save()
         cov.html_report()
     else:
         setup_folders(os.environ['RESULTS_PATH'])
-        save_name = args.save_name if args.save_name else datetime.now().strftime("%m%d%Y_%H%M%S")
+        if args.save_name:
+            save_name = args.save_name
+        else:
+            save_name = datetime.now().strftime("%m%d%Y_%H%M%S")
         OnAIR = ExecutionEngine(args.configfile, save_name, args.save)
         OnAIR.run_sim()
 
 
-
-""" Initializes global paths, used throughout execution """
 def init_global_paths(test=False):
-    run_path = 'onair/src/test' if test == True else './'
-    results_path = 'onair/src/test/results' if test == True else 'results/'
+    """
+    Initializes global paths, used throughout execution
+    """
+    run_path = 'onair/src/test' if test else './'
+    results_path = 'onair/src/test/results' if test else 'results/'
 
     os.environ['BASE_PATH'] = os.path.dirname(os.path.realpath(__file__))
-    os.environ['RUN_PATH'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), run_path)
-    os.environ['RESULTS_PATH'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), results_path)
+    os.environ['RUN_PATH'] = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), run_path)
+    os.environ['RESULTS_PATH'] = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), results_path)
     os.environ['SRC_ROOT_PATH'] = os.path.dirname(os.path.realpath(__file__))
 
-""" Disable terminal output """
+
 def blockPrint():
+    """ Disable terminal output """
     sys.stdout = open(os.devnull, 'w')
 
-""" Restore terminal output """
+
 def enablePrint():
+    """ Restore terminal output """
     sys.stdout = sys.__stdout__
 
 
