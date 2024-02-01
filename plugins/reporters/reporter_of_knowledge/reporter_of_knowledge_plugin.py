553fbfb42f88999e36101cf699fe431425b9cb0d
Alan Gibson <alan.s.gibson@nasa.gov>
2024-02-01 13:13:29 -0500

    Updates to get reporters running after rebase
    agent.py
      removed some trailing whitespaces
    reporters
      fixed naming of AIPlugin to match new usage

--- plugins/reporters/reporter_of_knowledge/reporter_of_knowledge_plugin.py
+++ plugins/reporters/reporter_of_knowledge/reporter_of_knowledge_plugin.py
@@ -7,9 +7,9 @@
 # Licensed under the NASA Open Source Agreement version 1.3
 # See "NOSA GSC-19165-1 OnAIR.pdf"
 
-from onair.src.ai_components.ai_plugin_abstract.core import AIPlugIn
+from onair.src.ai_components.ai_plugin_abstract.ai_plugin import AIPlugin
 
-class Plugin(AIPlugIn):
+class Plugin(AIPlugin):
     def update(self, low_level_data=[], high_level_data={}):
         """
         Given streamed data point, system should update internally
