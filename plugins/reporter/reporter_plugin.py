556b85f0e6bd4a419f2fdad7b200aab5bd01ea08
Alan Gibson <alan.s.gibson@nasa.gov>
2024-02-13 11:52:23 -0500

    Reduction to single reporter class
    Now that multiple use of same plugin can be done, only 1 reporter class required
    Now reports out via its name
    Reports function and data, low and high
      update reports data it was given and records it
      render_reasoning reports known data
    Added VERBOSITY constant for easy on/off of data reporting
      when True, all outputs
      when False, reporter only states function

---
+++ plugins/reporter/reporter_plugin.py
@@ -0,0 +1,36 @@
+# GSC-19165-1, "The On-Board Artificial Intelligence Research (OnAIR) Platform"
+#
+# Copyright © 2023 United States Government as represented by the Administrator of
+# the National Aeronautics and Space Administration. No copyright is claimed in the
+# United States under Title 17, U.S. Code. All Other Rights Reserved.
+#
+# Licensed under the NASA Open Source Agreement version 1.3
+# See "NOSA GSC-19165-1 OnAIR.pdf"
+
+from onair.src.ai_components.ai_plugin_abstract.ai_plugin import AIPlugin
+
+VERBOSE = False
+
+class Plugin(AIPlugin):
+    def update(self, low_level_data=[], high_level_data={}):
+        """
+        Reporter outputs that it is updating and outputs known headers and
+        given low and high level data.
+        """
+        print(f"{self.component_name}: UPDATE")
+        if VERBOSE:
+            print(f" : headers {self.headers}")
+            print(f" : low_level_data {low_level_data.__class__} = '{low_level_data}'")
+            self.low_level_data = low_level_data
+            print(f" : high_level_data {high_level_data.__class__} = '{high_level_data}'")
+            self.high_level_data = high_level_data
+
+    def render_reasoning(self):
+        """
+        Reporter outputs that it is reasoning and gives its known low and
+        high level data.
+        """
+        print(f"{self.component_name}: RENDER_REASONING")
+        if VERBOSE:
+            print(f" : My low_level_data is {self.low_level_data}")
+            print(f" : My high_level_data is {self.high_level_data}")
