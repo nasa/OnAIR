5d86c839c53e13d8d2a3e857610544389b148445
Alan Gibson <alan.s.gibson@nasa.gov>
2024-03-13 12:06:12 -0400

    Updates to enable multi-channel redis
    Updated CONFIG for use by redis-experiment-publisher
      shows a simple way to see data move through redis to OnAIR
    Added custom error to DataSource for key config errors
    Fixed columnization of redis_adapter
    Redis adapter now checks multiple channels
      detects when time is missing from received message
      helps detect unused keys
      helps detect missing expected keys
      warns if listener exits
    Updated and added necessary unit tests for coverage and code verification

--- onair/data_handling/redis_adapter.py
+++ onair/data_handling/redis_adapter.py
@@ -1,8 +1,9 @@
 # GSC-19165-1, "The On-Board Artificial Intelligence Research (OnAIR) Platform"
 #
-# Copyright © 2023 United States Government as represented by the Administrator of
-# the National Aeronautics and Space Administration. No copyright is claimed in the
-# United States under Title 17, U.S. Code. All Other Rights Reserved.
+# Copyright © 2023 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# No copyright is claimed in the United States under Title 17, U.S. Code.
+# All Other Rights Reserved.
 #
 # Licensed under the NASA Open Source Agreement version 1.3
 # See "NOSA GSC-19165-1 OnAIR.pdf"
@@ -19,6 +20,7 @@ import redis
 import json
 
 from onair.data_handling.on_air_data_source import OnAirDataSource
+from onair.data_handling.on_air_data_source import ConfigKeyError
 from onair.data_handling.tlm_json_parser import parseJson
 from onair.src.util.print_io import *
 from onair.data_handling.parser_util import *
@@ -30,12 +32,14 @@ class DataSource(OnAirDataSource):
         self.address = 'localhost'
         self.port = 6379
         self.db = 0
-        self.server = None  
+        self.server = None
         self.new_data_lock = threading.Lock()
         self.new_data = False
         self.currentData = []
-        self.currentData.append({'headers':None, 'data':None})
-        self.currentData.append({'headers':None, 'data':None})
+        self.currentData.append({'headers':self.order,
+                                 'data':list('-' * len(self.order))})
+        self.currentData.append({'headers':self.order,
+                                 'data':list('-' * len(self.order))})
         self.double_buffer_read_index = 0
         self.connect()
         self.subscribe(self.subscriptions)
@@ -63,8 +67,17 @@ class DataSource(OnAirDataSource):
             print_msg(f"No subscriptions given!")
 
     def parse_meta_data_file(self, meta_data_file, ss_breakdown):
-        configs = extract_meta_data_handle_ss_breakdown(meta_data_file, ss_breakdown)
+        configs = extract_meta_data_handle_ss_breakdown(
+            meta_data_file, ss_breakdown)
         meta = parseJson(meta_data_file)
+        keys = meta.keys()
+
+        if 'order' in keys:
+            self.order = meta['order']
+        else:
+            raise ConfigKeyError(f'Config file: \'{meta_data_file}\' ' \
+                                  'missing required key \'order\'')
+
         if 'redis_subscriptions' in meta.keys():
             self.subscriptions = meta['redis_subscriptions']
         else:
@@ -85,14 +98,15 @@ class DataSource(OnAirDataSource):
         while not data_available:
             with self.new_data_lock:
                 data_available = self.has_data()
-                
+
             if not data_available:
                 time.sleep(0.01)
 
         read_index = 0
         with self.new_data_lock:
             self.new_data = False
-            self.double_buffer_read_index = (self.double_buffer_read_index + 1) % 2
+            self.double_buffer_read_index = (
+                self.double_buffer_read_index + 1) % 2
             read_index = self.double_buffer_read_index
 
         return self.currentData[read_index]['data']
@@ -102,18 +116,61 @@ class DataSource(OnAirDataSource):
         return True
 
     def message_listener(self):
-        """Loop for listening for messages on channel"""
+        """Loop for listening for messages on channels"""
         for message in self.pubsub.listen():
             if message['type'] == 'message':
-                data = json.loads(message['data'])
-
-                currentData = self.currentData[(self.double_buffer_read_index + 1) %2]
-                currentData['headers'] = list(data.keys())
-                currentData['data'] = list(data.values())
-
+                channel_name = f"{message['channel'].decode()}"
+                # Attempt to load message as json
+                try:
+                    data = json.loads(message['data'])
+                except ValueError:
+                    # Warn of non-json conforming channel data received
+                    non_json_msg = f'Subscribed channel `{channel_name}\' ' \
+                                    'message received but is not in json ' \
+                                   f'format.\nMessage:\n{message["data"]}'
+                    print_msg(non_json_msg, ['WARNING'])
+                    continue
+                # Select the current data
+                currentData = self.currentData[
+                    (self.double_buffer_read_index + 1) % 2]
+                # turn all data points to unknown
+                currentData['data'] = ['-' for _ in currentData['data']]
+                # Find expected keys for received channel
+                expected_message_keys = \
+                    [k for k in currentData['headers'] if channel_name in k]
+                # Time is an expected key for all channels
+                expected_message_keys.append("time")
+                # Parse through the message keys for data points
+                for key in list(data.keys()):
+                    if key.lower() == 'time':
+                        header_string = key.lower()
+                    else:
+                        header_string = f"{channel_name}.{key}"
+                    # Look for channel specific values
+                    try:
+                        index = currentData['headers'].index(header_string)
+                        currentData['data'][index] = data[key]
+                        expected_message_keys.remove(header_string)
+                    # Unexpected key in data
+                    except ValueError:
+                        # warn user about key in data that is not in header
+                        print_msg(f"Unused key `{key}' in message " \
+                                  f'from channel `{channel_name}.\'',
+                                  ['WARNING'])
                 with self.new_data_lock:
                     self.new_data = True
+                # Warn user about expected keys missing from received data
+                for k in expected_message_keys:
+                    print_msg(f'Message from channel `{channel_name}\' ' \
+                              f'did not contain `{k}\' key\nMessage:\n' \
+                              f'{data}', ['WARNING'])
+            else:
+                # Warn user about non message receipts
+                print_msg(f"Redis adapter: channel " \
+                          f"'{message['channel'].decode()}' received " \
+                          f"message type: {message['type']}.", ['WARNING'])
+        # When listener loop exits warn user
+        print_msg("Redis subscription listener exited.", ['WARNING'])
 
     def has_data(self):
         return self.new_data
-    
