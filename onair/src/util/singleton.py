3ec99def6a1e71fdb521b19044110f50f5e7d537
dcrnasa <111317057+dcrnasa@users.noreply.github.com>
2024-11-08 10:35:54 -0500

    Remove extraneous comments from singleton class

--- onair/src/util/singleton.py
+++ onair/src/util/singleton.py
@@ -1,11 +1,3 @@
-# class Singleton(object):
-#     _instance = None
-#     def __new__(class_, *args, **kwargs):
-#         if not isinstance(class_._instance, class_):
-#             class_._instance = object.__new__(class_, *args, **kwargs)
-#         return class_._instance
-    
-
 class Singleton(object):
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, 'instance'):
