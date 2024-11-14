86269601d83dfc568250ea097fee1c7abf5276d9
TJ <timothy.b.chase@nasa.gov>
2024-11-14 14:07:25 -0500

    Black reformatted new services/unit testing code

--- onair/services/service_manager.py
+++ onair/services/service_manager.py
@@ -8,7 +8,9 @@ class ServiceManager(Singleton):
         if not hasattr(self, "_initialized"):
             # Ensure service info is provided on the first instantiation
             if service_dict is None:
-                raise ValueError("'service_dict' parameter required on first instantiation")
+                raise ValueError(
+                    "'service_dict' parameter required on first instantiation"
+                )
             services = import_services(service_dict)
             for service_name, service in services.items():
                 # Set attribute name = service name
@@ -20,12 +22,12 @@ class ServiceManager(Singleton):
         services = {}
         for service_name, service in vars(self).items():
             service_funcs = set()
-            if service_name.startswith('_'):
+            if service_name.startswith("_"):
                 # avoid "private" attributes, e.g. _initialized
                 continue
             for f in dir(service):
                 # only add the f if it's a function and not "private"
-                if callable(getattr(service, f)) and not f.startswith('_'):
+                if callable(getattr(service, f)) and not f.startswith("_"):
                     service_funcs.add(f)
             services[service_name] = service_funcs
 
--- test/onair/services/test_service_manager.py
+++ test/onair/services/test_service_manager.py
@@ -18,30 +18,37 @@ from onair.services.service_manager import ServiceManager
 import onair.services.service_manager as service_manager_import
 
 
-def test_ServiceManager__init__raises_ValueError_when_service_dict_is_None_on_first_instantiation(mocker):
+def test_ServiceManager__init__raises_ValueError_when_service_dict_is_None_on_first_instantiation(
+    mocker,
+):
     # Arrange / Act
     with pytest.raises(ValueError) as e_info:
         ServiceManager()
-    
+
     # Assert
-    assert str(e_info.value) == "'service_dict' parameter required on first instantiation"
+    assert (
+        str(e_info.value) == "'service_dict' parameter required on first instantiation"
+    )
 
 
 def test_ServiceManager__init__imports_services_and_sets_attributes(mocker):
     # Arrange
-    fake_service_dict = {'service1': {'path': 'path/to/service1'}, 'service2': {'path': 'path/to/service2'}}
-    fake_imported_services = {
-        'service1': MagicMock(),
-        'service2': MagicMock()
+    fake_service_dict = {
+        "service1": {"path": "path/to/service1"},
+        "service2": {"path": "path/to/service2"},
     }
-    mocker.patch('onair.services.service_manager.import_services', return_value=fake_imported_services)
+    fake_imported_services = {"service1": MagicMock(), "service2": MagicMock()}
+    mocker.patch(
+        "onair.services.service_manager.import_services",
+        return_value=fake_imported_services,
+    )
 
     # Act
     service_manager = ServiceManager(fake_service_dict)
 
     # Assert
-    assert service_manager.service1 == fake_imported_services['service1']
-    assert service_manager.service2 == fake_imported_services['service2']
+    assert service_manager.service1 == fake_imported_services["service1"]
+    assert service_manager.service2 == fake_imported_services["service2"]
     assert service_manager._initialized == True
 
     # Teardown
@@ -50,9 +57,11 @@ def test_ServiceManager__init__imports_services_and_sets_attributes(mocker):
 
 def test_ServiceManager__init__does_not_reinitialize_if_already_initialized(mocker):
     # Arrange
-    fake_service_dict = {'service1': {'path': 'path/to/service1'}}
-    mocker.patch.object(ServiceManager, '_initialized', True, create=True)
-    mock_import_services = mocker.patch('onair.src.util.service_import.import_services') # called in __init__
+    fake_service_dict = {"service1": {"path": "path/to/service1"}}
+    mocker.patch.object(ServiceManager, "_initialized", True, create=True)
+    mock_import_services = mocker.patch(
+        "onair.src.util.service_import.import_services"
+    )  # called in __init__
 
     # Act
     ServiceManager(fake_service_dict)
@@ -64,17 +73,21 @@ def test_ServiceManager__init__does_not_reinitialize_if_already_initialized(mock
     del ServiceManager.instance
 
 
-def test_ServiceManager_get_services_returns_dict_of_services_and_their_functions(mocker):
+def test_ServiceManager_get_services_returns_dict_of_services_and_their_functions(
+    mocker,
+):
     # Arrange
     class FakeService1:
         def func1(self):
             pass
+
         def _private_func(self):
             pass
 
     class FakeService2:
         def func2(self):
             pass
+
         def func3(self):
             pass
 
@@ -87,8 +100,8 @@ def test_ServiceManager_get_services_returns_dict_of_services_and_their_function
 
     # Assert
     assert result == {
-        'service1': {'func1'}, # correctly avoids _private_func
-        'service2': {'func2', 'func3'}
+        "service1": {"func1"},  # correctly avoids _private_func
+        "service2": {"func2", "func3"},
     }
 
     # Teardown
@@ -108,13 +121,18 @@ def test_ServiceManager_get_services_returns_empty_dict_when_no_services(mocker)
     # Teardown
     del ServiceManager.instance
 
-def test_ServiceManager_get_services_returns_empty_dict_and_does_reach_second_for_loop_when_own_items_returns_only_internal_or_private_attributes(mocker):
+
+def test_ServiceManager_get_services_returns_empty_dict_and_does_reach_second_for_loop_when_own_items_returns_only_internal_or_private_attributes(
+    mocker,
+):
     # Arrange
     service_manager = ServiceManager.__new__(ServiceManager)
     fake_vars_return = MagicMock()
     fake_internal_variable = MagicMock()
 
-    mocker.patch(service_manager_import.__name__ + ".vars", return_value=fake_vars_return)
+    mocker.patch(
+        service_manager_import.__name__ + ".vars", return_value=fake_vars_return
+    )
     mocker.patch(service_manager_import.__name__ + ".dir")
     fake_vars_return.items.return_value = iter([(fake_internal_variable, MagicMock())])
     fake_internal_variable.startswith.return_value = True
@@ -125,15 +143,18 @@ def test_ServiceManager_get_services_returns_empty_dict_and_does_reach_second_fo
     # Assert
     assert result == {}
     assert fake_internal_variable.startswith.call_count == 1
-    assert fake_internal_variable.startswith.call_args_list[0].args == ('_', )
+    assert fake_internal_variable.startswith.call_args_list[0].args == ("_",)
     assert service_manager_import.dir.call_count == 0
 
 
 def test_ServiceManager_behaves_as_singleton(mocker):
     # Arrange
-    fake_service_dict1 = {'service1': 'path1'}
-    fake_imported_service = {'service1': MagicMock()}
-    mocker.patch('onair.services.service_manager.import_services', return_value=fake_imported_service)
+    fake_service_dict1 = {"service1": "path1"}
+    fake_imported_service = {"service1": MagicMock()}
+    mocker.patch(
+        "onair.services.service_manager.import_services",
+        return_value=fake_imported_service,
+    )
 
     # Act
     service_manager1 = ServiceManager(fake_service_dict1)
@@ -141,7 +162,7 @@ def test_ServiceManager_behaves_as_singleton(mocker):
 
     # Assert
     assert service_manager1 is service_manager2
-    assert hasattr(service_manager2, 'service1')
+    assert hasattr(service_manager2, "service1")
 
     # Teardown
     del ServiceManager.instance
