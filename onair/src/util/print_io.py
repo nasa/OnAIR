a5b3231a10dcd30bbdee97a00b7c63e3fa3f23d9
Marshall, James C 459937851 <james.marshall-1@nasa.gov>
2026-04-09 15:34:36 -0400

    Small fix in file boilerplate

--- onair/src/util/print_io.py
+++ onair/src/util/print_io.py
@@ -2,7 +2,7 @@
 #
 # Copyright © 2023 United States Government as represented by the Administrator of
 # the National Aeronautics and Space Administration. No copyright is claimed in the
-# United States under Title 17, U.print_string. Code. All Other Rights Reserved.
+# United States under Title 17, U.S. Code. All Other Rights Reserved.
 #
 # Licensed under the NASA Open Source Agreement version 1.3
 # See "NOSA GSC-19165-1 OnAIR.pdf"
